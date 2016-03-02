1e873c123c469cefdd8808db85ebcdd8a797d43c
fillefilip8 <fillefilip8@gmail.com>
2016-03-02 10:37:43 +0100

    Added that fancy message when 1.9 clients try to enter a Minecart :)

--- src/main/java/us/myles/ViaVersion/ViaVersionPlugin.java
+++ src/main/java/us/myles/ViaVersion/ViaVersionPlugin.java
@@ -16,6 +16,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import us.myles.ViaVersion.api.ViaVersion;
 import us.myles.ViaVersion.api.ViaVersionAPI;
 import us.myles.ViaVersion.handlers.ViaVersionInitializer;
+import us.myles.ViaVersion.listeners.MinecartListener;
 import us.myles.ViaVersion.util.ReflectionUtil;
 
 import java.util.Collections;
@@ -52,6 +53,7 @@ public class ViaVersionPlugin extends JavaPlugin implements ViaVersionAPI {
                 setPorted(e.getPlayer().getUniqueId(), false);
             }
         }, this);
+        Bukkit.getPluginManager().registerEvents(new MinecartListener(),this);
     }
 
     public void injectPacketHandler() throws Exception {
