bb42cda4a8b716d721dd1a1626052e7be03845f5
fillefilip8 <fillefilip8@gmail.com>
2016-03-03 13:26:52 +0100

    .

---
+++ src/main/java/us/myles/ViaVersion/listeners/InteractListener.java
@@ -0,0 +1,15 @@
+package us.myles.ViaVersion.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+/**
+ * Created by fillefilip8 on 2016-03-03.
+ */
+public class InteractListener implements Listener {
+    @EventHandler
+    public void onSwordBlock(PlayerInteractEvent e){
+
+    }
+}
