704c876772078a29c5ba7aae2e7c529957f576aa
Myles <mylescomputer@gmail.com>
2016-03-19 20:45:07 +0000

    One line of code can make holes in the world, one line can fix them. (Fixes packet mem leak, also adds useful secret displayleaks command :) )

--- src/main/java/us/myles/ViaVersion/handlers/ViaDecodeHandler.java
+++ src/main/java/us/myles/ViaVersion/handlers/ViaDecodeHandler.java
@@ -47,6 +47,8 @@ public class ViaDecodeHandler extends ByteToMessageDecoder {
                 if (e.getCause() instanceof Exception) {
                     throw (Exception) e.getCause();
                 }
+            } finally {
+                bytebuf.release();
             }
         }
     }
