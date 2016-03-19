5ae8a3574077cfaf8ed474dc0dddd09f66f15d6c
BuildTools <unconfigured@null.spigotmc.org>
2016-03-19 01:11:16 +0100

    Change protocol constants to final fields

--- src/main/java/us/myles/ViaVersion/api/protocol/ProtocolVersion.java
+++ src/main/java/us/myles/ViaVersion/api/protocol/ProtocolVersion.java
@@ -2,11 +2,11 @@ package us.myles.ViaVersion.api.protocol;
 
 public class ProtocolVersion {
     /* Defined protocol constants */
-    public static int V1_7_1 = 4;
-    public static int V1_7_6 = 5;
+    public static final int V1_7_1 = 4;
+    public static final int V1_7_6 = 5;
 
-    public static int V1_8 = 47;
+    public static final int V1_8 = 47;
 
-    public static int V1_9 = 107;
-    public static int V1_9_1_PRE2 = 108;
+    public static final int V1_9 = 107;
+    public static final int V1_9_1_PRE2 = 108;
 }
