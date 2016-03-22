ccb2a4756f080cb2d22b0b670f051d5b37d8a636
Hugo Kerstens <hugokk@hotmail.nl>
2016-03-22 16:27:39 +0100

    Add config option to suppress 'Unable to get entity for ID' errors

--- src/main/java/us/myles/ViaVersion/protocols/protocol1_9to1_8/packets/EntityPackets.java
+++ src/main/java/us/myles/ViaVersion/protocols/protocol1_9to1_8/packets/EntityPackets.java
@@ -183,7 +183,7 @@ public class EntityPackets {
                         EntityTracker tracker = wrapper.user().get(EntityTracker.class);
                         if (tracker.getClientEntityTypes().containsKey(entityID)) {
                             MetadataRewriter.transform(tracker.getClientEntityTypes().get(entityID), metadataList);
-                        } else {
+                        } else if(!((ViaVersionPlugin)ViaVersion.getInstance()).getConfig().getBoolean("suppress-entityid-errors")){
                             System.out.println("Unable to find entity for metadata, entity ID: " + entityID);
                         }
                     }
