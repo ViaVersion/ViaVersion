45f70c35700d7f53339d6a930c90154a21c41a3b
Mats <matsvnl@gmail.com>
2016-05-16 13:59:30 +0200

    Translate update block entity to update sign. (#397)
    * Fix chunks
    
    * Translate update block entity to update sign

--- src/main/java/us/myles/ViaVersion/protocols/protocol1_9_1_2to1_9_3/Protocol1_9_1_2TO1_9_3.java
+++ src/main/java/us/myles/ViaVersion/protocols/protocol1_9_1_2to1_9_3/Protocol1_9_1_2TO1_9_3.java
@@ -1,7 +1,9 @@
 package us.myles.ViaVersion.protocols.protocol1_9_1_2to1_9_3;
 
+import org.spacehq.opennbt.tag.builtin.CompoundTag;
 import us.myles.ViaVersion.api.PacketWrapper;
 import us.myles.ViaVersion.api.data.UserConnection;
+import us.myles.ViaVersion.api.minecraft.Position;
 import us.myles.ViaVersion.api.protocol.Protocol;
 import us.myles.ViaVersion.api.remapper.PacketHandler;
 import us.myles.ViaVersion.api.remapper.PacketRemapper;
@@ -22,41 +24,31 @@ public class Protocol1_9_1_2TO1_9_3 extends Protocol {
         registerOutgoing(State.PLAY, 0x4A, 0x4B); //Entity properties
         registerOutgoing(State.PLAY, 0x4B, 0x4C); //Entity effect
 
-//        //Sign update
-//        registerOutgoing(State.PLAY, 0x46, -1, new PacketRemapper() {
-//            @Override
-//            public void registerMap() {
-//
-//                handler(new PacketHandler() {
-//                    @Override
-//                    public void handle(PacketWrapper wrapper) throws Exception {
-//                        //read data
-//                        Position position = wrapper.read(Type.POSITION);
-//                        String[] lines = new String[4];
-//                        for (int i = 0; i < 4; i++)
-//                            lines[i] = wrapper.read(Type.STRING);
-//
-//                        wrapper.clearInputBuffer();
-//
-//                        //write data
-//                        wrapper.setId(0x09); //Update block entity
-//                        wrapper.write(Type.POSITION, position); //Block location
-//                        wrapper.write(Type.UNSIGNED_BYTE, (short) 9); //Action type (9 update sign)
-//
-//                        //Create nbt
-//                        CompoundTag tag = new CompoundTag("");
-//                        tag.put(new StringTag("id", "Sign"));
-//                        tag.put(new IntTag("x", position.getX().intValue()));
-//                        tag.put(new IntTag("y", position.getY().intValue()));
-//                        tag.put(new IntTag("z", position.getZ().intValue()));
-//                        for (int i = 0; i < lines.length; i++)
-//                            tag.put(new StringTag("Text" + (i + 1), lines[i]));
-//
-//                        wrapper.write(Type.NBT, tag);
-//                    }
-//                });
-//            }
-//        });
+        //Update block entity
+        registerOutgoing(State.PLAY, 0x09, 0x09, new PacketRemapper() {
+            @Override
+            public void registerMap() {
+                map(Type.POSITION); //Position
+                map(Type.UNSIGNED_BYTE); //Type
+                map(Type.NBT); //NBT
+                handler(new PacketHandler() {
+                    @Override
+                    public void handle(PacketWrapper wrapper) throws Exception {
+                        if (wrapper.get(Type.UNSIGNED_BYTE, 0) == 9) {
+                            Position position = wrapper.get(Type.POSITION, 0);
+                            CompoundTag tag = wrapper.get(Type.NBT, 0);
+
+                            wrapper.clearPacket(); //Clear the packet
+
+                            wrapper.setId(0x46); //Update sign packet
+                            wrapper.write(Type.POSITION, position); // Position
+                            for (int i = 1; i < 5; i++)
+                                wrapper.write(Type.STRING, (String) tag.get("Text" + i).getValue()); // Sign line
+                        }
+                    }
+                });
+            }
+        });
 
         registerOutgoing(State.PLAY, 0x20, 0x20, new PacketRemapper() {
             @Override
