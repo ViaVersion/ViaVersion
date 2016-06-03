c9441de7e4c90c63527ddce3e2e1f019562ef28e
Matsv <admin@matsv.nl>
2016-06-03 21:15:53 +0200

    Change from info to severe

--- src/main/java/us/myles/ViaVersion/Bungee.java
+++ src/main/java/us/myles/ViaVersion/Bungee.java
@@ -8,11 +8,11 @@ public class Bungee extends Plugin {
 
     @Override
     public void onEnable() {
-        getLogger().info(RED + "===============================================");
-        getLogger().info(RED + "ViaVersion is NOT a Bungeecord plugin");
-        getLogger().info(RED + "Install this plugin on all your spigot/bukkit");
-        getLogger().info(RED + "servers and use the latest Bungeecord version");
-        getLogger().info(RED + "to make ViaVersion work with BungeeCord.");
-        getLogger().info(RED + "===============================================");
+        getLogger().severe(RED + "===============================================");
+        getLogger().severe(RED + "ViaVersion is NOT a Bungeecord plugin");
+        getLogger().severe(RED + "Install this plugin on all your spigot/bukkit");
+        getLogger().severe(RED + "servers and use the latest Bungeecord version");
+        getLogger().severe(RED + "to make ViaVersion work with BungeeCord.");
+        getLogger().severe(RED + "===============================================");
     }
 }
