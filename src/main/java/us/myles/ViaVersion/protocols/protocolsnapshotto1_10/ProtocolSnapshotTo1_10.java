44544925a13796a4ef7b6364d202d5e88a85bbba
Matsv <mats@matsv.nl>
2016-08-10 15:59:13 +0200

    map packet changes.

--- src/main/java/us/myles/ViaVersion/protocols/protocolsnapshotto1_10/ProtocolSnapshotTo1_10.java
+++ src/main/java/us/myles/ViaVersion/protocols/protocolsnapshotto1_10/ProtocolSnapshotTo1_10.java
@@ -1,12 +1,42 @@
 package us.myles.ViaVersion.protocols.protocolsnapshotto1_10;
 
+import us.myles.ViaVersion.api.PacketWrapper;
 import us.myles.ViaVersion.api.data.UserConnection;
 import us.myles.ViaVersion.api.protocol.Protocol;
+import us.myles.ViaVersion.api.remapper.PacketRemapper;
+import us.myles.ViaVersion.api.remapper.ValueCreator;
+import us.myles.ViaVersion.api.type.Type;
+import us.myles.ViaVersion.packets.State;
 
 public class ProtocolSnapshotTo1_10 extends Protocol {
     @Override
     protected void registerPackets() {
+        // Spawn mob packet
+        registerOutgoing(State.PLAY, 0x03, 0x03, new PacketRemapper() {
+            @Override
+            public void registerMap() {
+                map(Type.VAR_INT); // 0 - Entity ID
+                map(Type.UUID); // 1 - Entity UUID
+                map(Type.UNSIGNED_BYTE, Type.VAR_INT); // 2 - Entity Type
 
+            }
+        });
+
+        // Collect item packet
+        registerOutgoing(State.PLAY, 0x48, 0x48, new PacketRemapper() {
+            @Override
+            public void registerMap() {
+                map(Type.VAR_INT); // 0 - Collected entity id
+                map(Type.VAR_INT); // 1 - Collector entity id
+
+                create(new ValueCreator() {
+                    @Override
+                    public void write(PacketWrapper wrapper) throws Exception {
+                        // TODO FIND OUT WHAT THE NEW VAR_INT IS
+                    }
+                });
+            }
+        });
     }
 
     @Override
