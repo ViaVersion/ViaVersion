11d26c13b83b90091aad6efdf93b965c4b27b551
Matsv <mats@matsv.nl>
2017-05-10 18:03:15 +0200

    1.12-pre1

--- common/src/main/java/us/myles/ViaVersion/api/protocol/ProtocolVersion.java
+++ common/src/main/java/us/myles/ViaVersion/api/protocol/ProtocolVersion.java
@@ -55,7 +55,7 @@ public class ProtocolVersion {
         register(v1_10 = new ProtocolVersion(210, "1.10"));
         register(v1_11 = new ProtocolVersion(315, "1.11"));
         register(v1_11_1 = new ProtocolVersion(316, "1.11.1"));
-        register(snapshot = new ProtocolVersion(327, "17w18b"));
+        register(snapshot = new ProtocolVersion(328, "1.12-pre1"));
         register(unknown = new ProtocolVersion(-1, "UNKNOWN"));
     }
 
