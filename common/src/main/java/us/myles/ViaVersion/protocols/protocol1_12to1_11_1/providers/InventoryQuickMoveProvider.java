c46bdcf42bc02f12cfaa48307e490df46919c3e9
mmxw11 <mmxw11@users.noreply.github.com>
2018-01-22 21:32:22 +0200

    Fix inventory handling
    fixes #780 & #800

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_12to1_11_1/providers/InventoryQuickMoveProvider.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_12to1_11_1/providers/InventoryQuickMoveProvider.java
@@ -5,7 +5,7 @@ import us.myles.ViaVersion.api.platform.providers.Provider;
 
 public class InventoryQuickMoveProvider implements Provider {
 
-    public boolean registerQuickMove(short windowId, short slotId, short actionId, UserConnection userConnection) {
+    public boolean registerQuickMoveAction(short windowId, short slotId, short actionId, UserConnection userConnection) {
         return false; // not supported :/ plays very sad violin
     }
 }
