2fb953357582bbbb4f785d33867f6526b1677ce9
Matsv <mats@matsv.nl>
2018-03-17 17:25:35 +0100

    Auto-generate entity id changes to prepare for PAaaS EnityTypes generation

--- common/src/main/java/us/myles/ViaVersion/protocols/protocolsnapshotto1_12_2/packets/EntityPackets.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocolsnapshotto1_12_2/packets/EntityPackets.java
@@ -2,7 +2,7 @@ package us.myles.ViaVersion.protocols.protocolsnapshotto1_12_2.packets;
 
 import com.google.common.base.Optional;
 import us.myles.ViaVersion.api.PacketWrapper;
-import us.myles.ViaVersion.api.entities.Entity1_12Types;
+import us.myles.ViaVersion.api.Via;
 import us.myles.ViaVersion.api.entities.Entity1_13Types;
 import us.myles.ViaVersion.api.protocol.Protocol;
 import us.myles.ViaVersion.api.remapper.PacketHandler;
@@ -12,6 +12,7 @@ import us.myles.ViaVersion.api.type.types.version.Types1_12;
 import us.myles.ViaVersion.api.type.types.version.Types1_13;
 import us.myles.ViaVersion.packets.State;
 import us.myles.ViaVersion.protocols.protocolsnapshotto1_12_2.MetadataRewriter;
+import us.myles.ViaVersion.protocols.protocolsnapshotto1_12_2.data.EntityTypeRewriter;
 import us.myles.ViaVersion.protocols.protocolsnapshotto1_12_2.storage.EntityTracker;
 
 public class EntityPackets {
@@ -25,22 +26,24 @@ public class EntityPackets {
                 map(Type.VAR_INT); // 0 - Entity id
                 map(Type.UUID); // 1 - UUID
                 map(Type.BYTE); // 2 - Type
-                // TODO handle new 1.13 entities
+
                 // Track Entity
                 handler(new PacketHandler() {
                     @Override
                     public void handle(PacketWrapper wrapper) throws Exception {
                         int entityId = wrapper.get(Type.VAR_INT, 0);
                         byte type = wrapper.get(Type.BYTE, 0);
+                        Optional<Entity1_13Types.ObjectTypes> entType = Entity1_13Types.ObjectTypes.findById(type);
 
-                        Entity1_13Types.EntityType entType = Entity1_13Types.EntityType.valueOf(
-                                Entity1_12Types.getTypeFromId(type, true).name()
-                        );
+                        if (!entType.isPresent()) {
+                            Via.getPlatform().getLogger().severe("No object type id registered! " + type);
+                            return;
+                        }
 
-                        wrapper.set(Type.BYTE, 0, (byte) entType.getId());
+                        // TODO no object type changes so far
 
                         // Register Type ID
-                        wrapper.user().get(EntityTracker.class).addEntity(entityId, entType);
+                        wrapper.user().get(EntityTracker.class).addEntity(entityId, entType.get().getType());
                     }
                 });
             }
@@ -70,11 +73,13 @@ public class EntityPackets {
                         int entityId = wrapper.get(Type.VAR_INT, 0);
                         int type = wrapper.get(Type.VAR_INT, 1);
 
-                        Entity1_13Types.EntityType entType = Entity1_13Types.EntityType.valueOf(
-                                Entity1_12Types.getTypeFromId(type, false).name()
-                        );
+                        Optional<Integer> optNewType = EntityTypeRewriter.getNewId(type);
+                        type = optNewType.or(type);
+                        Entity1_13Types.EntityType entType = Entity1_13Types.getTypeFromId(type, false);
 
-                        wrapper.set(Type.VAR_INT, 1, type = entType.getId());
+
+                        wrapper.set(Type.VAR_INT, 1, type);
+
 
                         // Register Type ID
                         wrapper.user().get(EntityTracker.class).addEntity(entityId, entType);
