7a01376144751c7ba95dae04f38deff3ca5ba0c2
Matsv <mats@matsv.nl>
2018-03-18 14:11:40 +0100

    Generate Entity1_13Types

--- common/src/main/java/us/myles/ViaVersion/api/entities/Entity1_13Types.java
+++ common/src/main/java/us/myles/ViaVersion/api/entities/Entity1_13Types.java
@@ -27,122 +27,145 @@ public class Entity1_13Types {
     @AllArgsConstructor
     @Getter
     public enum EntityType {
-        ENTITY(-1),
-        DROPPED_ITEM(30, ENTITY),
-        EXPERIENCE_ORB(20, ENTITY),
-        LEASH_HITCH(33, ENTITY), // Actually entity hanging but it doesn't make a lot of difference for metadata
-        PAINTING(47, ENTITY), // Actually entity hanging but it doesn't make a lot of difference for metadata
-        ARROW(2, ENTITY),
-        SNOWBALL(65, ENTITY), // Actually EntityProjectile
-        FIREBALL(32, ENTITY),
-        SMALL_FIREBALL(63, ENTITY),
-        ENDER_PEARL(72, ENTITY), // Actually EntityProjectile
-        ENDER_SIGNAL(21, ENTITY),
-        THROWN_EXP_BOTTLE(73, ENTITY),
-        ITEM_FRAME(31, ENTITY), // Actually EntityHanging
-        WITHER_SKULL(82, ENTITY),
-        PRIMED_TNT(53, ENTITY),
-        FALLING_BLOCK(22, ENTITY),
-        FIREWORK(23, ENTITY),
-        SPECTRAL_ARROW(66, ARROW),
-        SHULKER_BULLET(58, ENTITY),
-        DRAGON_FIREBALL(12, FIREBALL),
-        EVOCATION_FANGS(18, ENTITY),
+        // Auto generated
 
+        ENTITY(-1), // abm
 
-        ENTITY_LIVING(-1, ENTITY),
-        ENTITY_INSENTIENT(-1, ENTITY_LIVING),
-        ENTITY_AGEABLE(-1, ENTITY_INSENTIENT),
-        ENTITY_TAMEABLE_ANIMAL(-1, ENTITY_AGEABLE),
-        ENTITY_HUMAN(-1, ENTITY_LIVING),
+        AREA_EFFECT_CLOUD(0, ENTITY), // abk
+        ENDER_CRYSTAL(15, ENTITY), // aho
+        EVOCATION_FANGS(19, ENTITY), // ala
+        XP_ORB(21, ENTITY), // abs
+        EYE_OF_ENDER_SIGNAL(22, ENTITY), // alb
+        FALLING_BLOCK(23, ENTITY), // aix
+        FIREWORKS_ROCKET(24, ENTITY), // alc
+        ITEM(31, ENTITY), // aiy
+        LLAMA_SPIT(36, ENTITY), // ale
+        TNT(54, ENTITY), // aiz
+        SHULKER_BULLET(59, ENTITY), // alh
+        FISHING_BOBBER(92, ENTITY), // ais
 
-        ARMOR_STAND(1, ENTITY_LIVING),
-        ENTITY_ILLAGER_ABSTRACT(-1, ENTITY_INSENTIENT),
-        EVOCATION_ILLAGER(19, ENTITY_ILLAGER_ABSTRACT),
-        VEX(75, ENTITY_INSENTIENT),
-        VINDICATION_ILLAGER(78, ENTITY_ILLAGER_ABSTRACT),
-        ILLUSION_ILLAGER(29, EVOCATION_ILLAGER),
+        LIVINGENTITY(-1, ENTITY), // abv
+
+        PLAYER(91, LIVINGENTITY), // aks
+
+        ABW(-1, LIVINGENTITY), // abw
+        ACD(-1, ABW), // acd
+        ABJ(-1, ACD), // abj
+        AGD(-1, ABJ), // agd
+        ACG(-1, AGD), // acg
+        AGR(-1, ACG), // agr
+
+        PARROT(49, AGR), // agk
+        OCELOT(47, ACG), // agj
+        WOLF(85, ACG), // agy
+        CHICKEN(7, AGD), // age
+        COW(9, AGD), // agg
+        MOOSHROOM(46, COW), // agi
+        POLAR_BEAR(53, AGD), // agm
+        PIG(50, AGD), // agl
+        RABBIT(55, AGD), // ago
+        SHEEP(57, AGD), // agq
+        TURTLE(72, AGD), // agv
+
+        // Horses
+        ABSTRACT_HORSE(-1, AGD), // aha
+        CHESTED_HORSE(-1, ABSTRACT_HORSE), // agz
+        DONKEY(11, CHESTED_HORSE), // ahb
+        MULE(45, CHESTED_HORSE), // ahf
+        LLAMA(35, CHESTED_HORSE), // ahe
+        HORSE(28, ABSTRACT_HORSE), // ahc
+        SKELETON_HORSE(62, ABSTRACT_HORSE), // ahg
+        ZOMBIE_HORSE(87, ABSTRACT_HORSE), // ahi
+
+        VILLAGER(78, ABJ), // akn
+
+        // Golem
+        ABSTRACT_GOLEM(-1, ACD), // agc
+        SNOWMAN(65, ABSTRACT_GOLEM), // ags
+        VILLAGER_GOLEM(79, ABSTRACT_GOLEM), // agw
+        SHULKER(58, ABSTRACT_GOLEM), // ajx
+
+        AGB(-1, ACD), // agb
+        COD_MOB(8, AGB), // agf
+        PUFFER_FISH(51, AGB), // agn
+        SALMON_MOB(56, AGB), // agp
+        TROPICAL_FISH(71, AGB), // agu
+        AJS(-1, ACD), // ajs
+        WITHER(82, AJS), // aij
+        AJB(-1, AJS), // ajb
+        AKB(-1, AJB), // akb
+        EVOCATION_ILLAGER(20, AKB), // ajl
+        ILLUSION_ILLAGER(30, AKB), // ajq
+        VINDICATION_ILLAGER(80, AJB), // akf
+        BLAZE(4, AJS), // ajd
+        AJC(-1, AJS), // ajc
+        SKELETON(61, AJC), // ajz
+        STRAY(70, AJC), // akd
+        WITHER_SKELETON(83, AJC), // akh
+        CREEPER(10, AJS), // ajf
+        SPIDER(68, AJS), // akc
+        CAVE_SPIDER(6, SPIDER), // aje
+        GUARDIAN(27, AJS), // ajo
+        ELDER_GUARDIAN(14, GUARDIAN), // ajh
+        ZOMBIE(86, AJS), // aki
+        DROWNED(13, ZOMBIE), // ajg
+        HUSK(29, ZOMBIE), // ajp
+        ZOMBIE_PIGMAN(52, ZOMBIE), // aju
+        ZOMBIE_VILLAGER(88, ZOMBIE), // akj
+        ENDERMITE(18, AJS), // ajj
+        ENDERMAN(17, AJS), // aji
+        GIANT(26, AJS), // ajn
+        SILVERFISH(60, AJS), // ajy
+        VEX(77, AJS), // ake
+        WITCH(81, AJS), // akg
+        ABT(-1, ABW), // abt
+        GHAST(25, ABT), // ajm
+        PHANTOM(89, ABT), // ajt
+        AFY(-1, ABW), // afy
+        BAT(3, AFY), // afz
+        AGX(-1, ABW), // agx
+        SQUID(69, AGX), // agt
+        ENDER_DRAGON(16, ABW), // ahp
+        SLIME(63, ABW), // aka
+        MAGMA_CUBE(37, SLIME), // ajr
+        ARMOR_STAND(1, LIVINGENTITY), // ail
+        AHN(-1, ENTITY), // ahn
+        AIM(-1, ENTITY), // aim
+        LEASH_KNOT(34, AIM), // aio
+        ITEM_FRAME(32, AIM), // ain
+        PAINTING(48, AIM), // aiq
+        AIU(-1, ENTITY), // aiu
+        LIGHTNING_BOLT(90, AIU), // aiv
+        AKW(-1, ENTITY), // akw
+        ARROW(2, AKW), // aky
+        SPECTRAL_ARROW(67, AKW), // alk
+        TRIDENT(93, AKW), // alq
+        AKX(-1, ENTITY), // akx
+        DRAGON_FIREBALL(12, AKX), // akz
+        FIREBALL(33, AKX), // ald
+        SMALL_FIREBALL(64, AKX), // ali
+        WITHER_SKULL(84, AKX), // alr
+
+        // Projectiles
+        PROJECTILE_ABSTRACT(-1, ENTITY), // all
+        SNOWBALL(66, PROJECTILE_ABSTRACT), // alj
+        ENDER_PEARL(74, PROJECTILE_ABSTRACT), // aln
+        EGG(73, PROJECTILE_ABSTRACT), // alm
+        POTION(76, PROJECTILE_ABSTRACT), // alp
+        XP_BOTTLE(75, PROJECTILE_ABSTRACT), // alo
 
         // Vehicles
-        MINECART_ABSTRACT(-1, ENTITY),
-        MINECART_COMMAND(39, MINECART_ABSTRACT),
-        BOAT(5, ENTITY),
-        MINECART_RIDEABLE(37, MINECART_ABSTRACT),
-        MINECART_CHEST(38, MINECART_ABSTRACT),
-        MINECART_FURNACE(40, MINECART_ABSTRACT),
-        MINECART_TNT(43, MINECART_ABSTRACT),
-        MINECART_HOPPER(41, MINECART_ABSTRACT),
-        MINECART_MOB_SPAWNER(42, MINECART_ABSTRACT),
+        MINECART_ABSTRACT(-1, ENTITY), // alt
+        CHESTED_MINECART_ABSTRACT(-1, MINECART_ABSTRACT), // alu
+        CHEST_MINECART(39, CHESTED_MINECART_ABSTRACT), // alx
+        HOPPER_MINECART(42, CHESTED_MINECART_ABSTRACT), // ama
+        MINECART(38, MINECART_ABSTRACT), // alw
+        FURNACE_MINECART(41, MINECART_ABSTRACT), // alz
+        COMMANDBLOCK_MINECART(40, MINECART_ABSTRACT), // aly
+        TNT_MINECART(44, MINECART_ABSTRACT), // amc
+        SPAWNER_MINECART(43, MINECART_ABSTRACT), // amb
+        BOAT(5, ENTITY); // alv
 
-        CREEPER(10, ENTITY_INSENTIENT),
-
-        ABSTRACT_SKELETON(-1, ENTITY_INSENTIENT),
-        SKELETON(60, ABSTRACT_SKELETON),
-        WITHER_SKELETON(81, ABSTRACT_SKELETON),
-        STRAY(69, ABSTRACT_SKELETON),
-
-        SPIDER(67, ENTITY_INSENTIENT),
-        GIANT(25, ENTITY_INSENTIENT),
-
-        ZOMBIE(84, ENTITY_INSENTIENT),
-        HUSK(28, ZOMBIE),
-        ZOMBIE_VILLAGER(86, ZOMBIE),
-
-        SLIME(62, ENTITY_INSENTIENT),
-        GHAST(24, ENTITY_INSENTIENT),
-        PIG_ZOMBIE(51, ZOMBIE),
-        ENDERMAN(16, ENTITY_INSENTIENT),
-        CAVE_SPIDER(6, SPIDER),
-        SILVERFISH(59, ENTITY_INSENTIENT),
-        BLAZE(4, ENTITY_INSENTIENT),
-        MAGMA_CUBE(36, SLIME),
-        ENDER_DRAGON(15, ENTITY_INSENTIENT),
-        WITHER(80, ENTITY_INSENTIENT),
-        BAT(3, ENTITY_INSENTIENT),
-        WITCH(79, ENTITY_INSENTIENT),
-        ENDERMITE(17, ENTITY_INSENTIENT),
-
-        GUARDIAN(26, ENTITY_INSENTIENT),
-        ELDER_GUARDIAN(13, GUARDIAN), // Moved down to avoid illegal forward reference
-
-        IRON_GOLEM(77, ENTITY_INSENTIENT), // moved up to avoid illegal forward references
-        SHULKER(57, IRON_GOLEM),
-        PIG(49, ENTITY_AGEABLE),
-        SHEEP(56, ENTITY_AGEABLE),
-        COW(9, ENTITY_AGEABLE),
-        CHICKEN(7, ENTITY_AGEABLE),
-        SQUID(68, ENTITY_INSENTIENT),
-        WOLF(83, ENTITY_TAMEABLE_ANIMAL),
-        MUSHROOM_COW(45, COW),
-        SNOWMAN(64, IRON_GOLEM),
-        OCELOT(46, ENTITY_TAMEABLE_ANIMAL),
-        PARROT(48, ENTITY_TAMEABLE_ANIMAL),
-
-        ABSTRACT_HORSE(-1, ENTITY_AGEABLE),
-        HORSE(27, ABSTRACT_HORSE),
-        SKELETON_HORSE(61, ABSTRACT_HORSE),
-        ZOMBIE_HORSE(85, ABSTRACT_HORSE),
-
-        CHESTED_HORSE(-1, ABSTRACT_HORSE),
-        DONKEY(11, CHESTED_HORSE),
-        MULE(44, CHESTED_HORSE),
-        LIAMA(34, CHESTED_HORSE),
-
-
-        RABBIT(54, ENTITY_AGEABLE),
-        POLAR_BEAR(52, ENTITY_AGEABLE),
-        VILLAGER(76, ENTITY_AGEABLE),
-        ENDER_CRYSTAL(14, ENTITY),
-        SPLASH_POTION(-1, ENTITY),
-        LINGERING_POTION(-1, SPLASH_POTION),
-        AREA_EFFECT_CLOUD(-1, ENTITY),
-        EGG(-1, ENTITY),
-        FISHING_HOOK(-1, ENTITY),
-        LIGHTNING(-1, ENTITY),
-        WEATHER(-1, ENTITY),
-        PLAYER(-1, ENTITY_HUMAN),
-        COMPLEX_PART(-1, ENTITY),
-        LIAMA_SPIT(-1, ENTITY);
 
         private final int id;
         private final EntityType parent;
@@ -192,10 +215,10 @@ public class Entity1_13Types {
     @Getter
     public enum ObjectTypes {
         BOAT(1, EntityType.BOAT),
-        ITEM(2, EntityType.DROPPED_ITEM),
+        ITEM(2, EntityType.ITEM),
         AREA_EFFECT_CLOUD(3, EntityType.AREA_EFFECT_CLOUD),
         MINECART(10, EntityType.MINECART_ABSTRACT),
-        TNT_PRIMED(50, EntityType.PRIMED_TNT),
+        TNT_PRIMED(50, EntityType.TNT),
         ENDER_CRYSTAL(51, EntityType.ENDER_CRYSTAL),
         TIPPED_ARROW(60, EntityType.ARROW),
         SNOWBALL(61, EntityType.SNOWBALL),
@@ -205,17 +228,17 @@ public class Entity1_13Types {
         ENDER_PEARL(65, EntityType.ENDER_PEARL),
         WITHER_SKULL(66, EntityType.WITHER_SKULL),
         SHULKER_BULLET(67, EntityType.SHULKER_BULLET),
-        LIAMA_SPIT(68, EntityType.LIAMA_SPIT),
+        LIAMA_SPIT(68, EntityType.LLAMA_SPIT),
         FALLING_BLOCK(70, EntityType.FALLING_BLOCK),
         ITEM_FRAME(71, EntityType.ITEM_FRAME),
-        ENDER_SIGNAL(72, EntityType.ENDER_SIGNAL),
-        POTION(73, EntityType.SPLASH_POTION),
-        THROWN_EXP_BOTTLE(75, EntityType.THROWN_EXP_BOTTLE),
-        FIREWORK(76, EntityType.FIREWORK),
-        LEASH(77, EntityType.LEASH_HITCH),
+        ENDER_SIGNAL(72, EntityType.EYE_OF_ENDER_SIGNAL),
+        POTION(73, EntityType.POTION),
+        THROWN_EXP_BOTTLE(75, EntityType.XP_BOTTLE),
+        FIREWORK(76, EntityType.FIREWORKS_ROCKET),
+        LEASH(77, EntityType.LEASH_KNOT),
         ARMOR_STAND(78, EntityType.ARMOR_STAND),
         EVOCATION_FANGS(79, EntityType.EVOCATION_FANGS),
-        FISHIHNG_HOOK(90, EntityType.FISHING_HOOK),
+        FISHIHNG_HOOK(90, EntityType.FISHING_BOBBER),
         SPECTRAL_ARROW(91, EntityType.SPECTRAL_ARROW),
         DRAGON_FIREBALL(93, EntityType.DRAGON_FIREBALL);
 
