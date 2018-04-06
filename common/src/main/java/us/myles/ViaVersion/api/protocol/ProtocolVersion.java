f2f485731fbc34ececa3105eaf560a5a0482e753
Matsv <mats@matsv.nl>
2018-04-06 19:52:45 +0200

    Update to 18w14b

--- common/src/main/java/us/myles/ViaVersion/api/protocol/ProtocolVersion.java
+++ common/src/main/java/us/myles/ViaVersion/api/protocol/ProtocolVersion.java
@@ -62,7 +62,7 @@ public class ProtocolVersion {
         register(v1_12 = new ProtocolVersion(335, "1.12"));
         register(v1_12_1 = new ProtocolVersion(338, "1.12.1"));
         register(v1_12_2 = new ProtocolVersion(340, "1.12.2"));
-        register(v1_13 = new ProtocolVersion(369, "18w14a"));
+        register(v1_13 = new ProtocolVersion(370, "18w14b"));
         register(unknown = new ProtocolVersion(-1, "UNKNOWN"));
     }
 
