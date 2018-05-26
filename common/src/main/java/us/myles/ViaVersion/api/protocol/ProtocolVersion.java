9d39bb2d621561792f61f3de3ae5e8bd51cc9b58
creeper123123321 <creeper123123321@gmail.com>
2018-05-26 08:52:31 -0300

    18w21b

--- common/src/main/java/us/myles/ViaVersion/api/protocol/ProtocolVersion.java
+++ common/src/main/java/us/myles/ViaVersion/api/protocol/ProtocolVersion.java
@@ -62,7 +62,7 @@ public class ProtocolVersion {
         register(v1_12 = new ProtocolVersion(335, "1.12"));
         register(v1_12_1 = new ProtocolVersion(338, "1.12.1"));
         register(v1_12_2 = new ProtocolVersion(340, "1.12.2"));
-        register(v1_13 = new ProtocolVersion(378, "18w21a"));
+        register(v1_13 = new ProtocolVersion(379, "18w21b"));
         register(unknown = new ProtocolVersion(-1, "UNKNOWN"));
     }
 
