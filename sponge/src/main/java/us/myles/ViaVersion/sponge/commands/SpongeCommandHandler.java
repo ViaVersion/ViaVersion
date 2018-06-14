b20dad57feaac2f07c34e78f9b65e7a83f037fc0
creeper123123321 <creeper123123321@gmail.com>
2018-06-14 13:24:42 -0300

    Fix tab completion on Sponge

--- sponge/src/main/java/us/myles/ViaVersion/sponge/commands/SpongeCommandHandler.java
+++ sponge/src/main/java/us/myles/ViaVersion/sponge/commands/SpongeCommandHandler.java
@@ -27,7 +27,7 @@ public class SpongeCommandHandler extends ViaCommandHandler implements CommandCa
     }
 
     public List<String> getSuggestions(CommandSource source, String arguments) throws CommandException {
-        String[] args = arguments.length() > 0 ? arguments.split(" ") : new String[0];
+        String[] args = arguments.split(" "); // ViaCommandHandler handles empty String in array
         return onTabComplete(new SpongeCommandSender(source), args);
     }
 
