5d19697004041b54242154ec7cc3523d08d3f3f8
creeper123123321 <creeper123123321@gmail.com>
2018-07-18 14:43:31 -0300

    Fix patterns

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/providers/blockentities/BannerHandler.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/providers/blockentities/BannerHandler.java
@@ -1,6 +1,8 @@
 package us.myles.ViaVersion.protocols.protocol1_13to1_12_2.providers.blockentities;
 
 import com.github.steveice10.opennbt.tag.builtin.CompoundTag;
+import com.github.steveice10.opennbt.tag.builtin.IntTag;
+import com.github.steveice10.opennbt.tag.builtin.ListTag;
 import com.github.steveice10.opennbt.tag.builtin.Tag;
 import us.myles.ViaVersion.api.data.UserConnection;
 import us.myles.ViaVersion.api.minecraft.Position;
@@ -35,6 +37,15 @@ public class BannerHandler implements BlockEntityProvider.BlockEntityHandler {
             blockId += ((15 - color) * 4);
         else
             System.out.println("Why does this block have the banner block entity? :(" + tag);
+
+        if (tag.get("Patterns") instanceof ListTag) {
+            for (Tag pattern : (ListTag) tag.get("Patterns")) {
+                if (pattern instanceof CompoundTag) {
+                    IntTag c = ((CompoundTag) pattern).get("Color");
+                    c.setValue(15 - c.getValue()); // Invert color id
+                }
+            }
+        }
 
         return blockId;
     }
