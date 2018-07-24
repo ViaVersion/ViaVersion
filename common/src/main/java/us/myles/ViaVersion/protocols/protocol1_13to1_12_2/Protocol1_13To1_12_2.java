a403c7424c1411018c6577dc43a39526edd2bc4d
creeper123123321 <creeper123123321@gmail.com>
2018-07-24 10:55:08 -0300

    *facepalm*

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/Protocol1_13To1_12_2.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/Protocol1_13To1_12_2.java
@@ -226,7 +226,7 @@ public class Protocol1_13To1_12_2 extends Protocol {
                         if (id == 1010) { // Play record
                             wrapper.set(Type.INT, 1, data = MappingData.oldToNewItems.get(data << 4));
                         } else if (id == 2001) { // Block break + block break sound
-                            int blockId = 12 & 0xFFF;
+                            int blockId = data & 0xFFF;
                             int blockData = data >> 12;
                             wrapper.set(Type.INT, 1, data = WorldPackets.toNewId(blockId << 4 | blockData));
                         }
