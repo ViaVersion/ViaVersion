9f4aa53ecd71e7e687b94f2c510d3f3204fd4492
Marco Neuhaus <mneuhaus44@gmail.com>
2018-09-23 17:53:37 +0200

    add fix cobblestone wall

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/FenceConnectionHandler.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/FenceConnectionHandler.java
@@ -3,13 +3,13 @@ package us.myles.ViaVersion.protocols.protocol1_13to1_12_2.blockconnections;
 import us.myles.ViaVersion.api.minecraft.BlockFace;
 import us.myles.ViaVersion.api.minecraft.Position;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
 class FenceConnectionHandler implements ConnectionHandler {
 	private static HashSet<String> baseFences = new HashSet<>();
 	private static Map<Integer, String> fences = new HashMap<>();
+	
+	private static final List<BlockFace> blockFaceList = Arrays.asList(BlockFace.EAST, BlockFace.NORTH, BlockFace.SOUTH, BlockFace.WEST);
 
 	static void init() {
 		baseFences.add("minecraft:oak_fence");
@@ -36,7 +36,7 @@ class FenceConnectionHandler implements ConnectionHandler {
 	    String key = fences.get(blockState);
 	    String extraData = "";
 	    if(key.endsWith("_wall")){
-	        extraData = "up=" + key.equals(fences.get(connectionData.get(position.getRelative(BlockFace.TOP)))) + ",";
+	        extraData = "up=" + up(position, connectionData) + ",";
         }
 		String data = key + '[' +
 				             "east=" + connects(BlockFace.EAST, connectionData.get(position.getRelative(BlockFace.EAST))) + ',' +
@@ -52,4 +52,31 @@ class FenceConnectionHandler implements ConnectionHandler {
 	private boolean connects(BlockFace side, int blockState) {
 		return fences.containsKey(blockState) || ConnectionData.blockConnectionData.containsKey(blockState) && ConnectionData.blockConnectionData.get(blockState).connectsToFences(side.opposite());
 	}
+
+	public boolean up(Position position, ConnectionData data){
+	    List<BlockFace> list = getBlockFaces(position, data);
+	    if(list.isEmpty()) return true;
+	    if(list.size() >= 4) return true;
+        for (BlockFace blockFace : list) {
+            if(!list.contains(blockFace.opposite())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private List<BlockFace> getBlockFaces(Position position, ConnectionData data){
+	    List<BlockFace> blockFaces = new ArrayList<>();
+        for (BlockFace face : blockFaceList) {
+            if(isWall(data.get(position.getRelative(face)))){
+                blockFaces.add(face);
+            }
+        }
+        return blockFaces;
+    }
+
+    private boolean isWall(int id){
+	    return fences.get(id).endsWith("_wall");
+    }
+
 }
