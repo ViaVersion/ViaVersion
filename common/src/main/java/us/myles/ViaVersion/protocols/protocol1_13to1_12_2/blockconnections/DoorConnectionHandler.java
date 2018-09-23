7f13c8134338a43c02c3b82ec3c8c96e3cc3bffc
Marco Neuhaus <mneuhaus44@gmail.com>
2018-09-23 16:49:12 +0200

    fix door bug

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/DoorConnectionHandler.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/DoorConnectionHandler.java
@@ -6,22 +6,23 @@ import us.myles.ViaVersion.api.minecraft.Position;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Marco Neuhaus on 23.09.2018 for the Project ViaVersionGerry.
  */
 public class DoorConnectionHandler implements ConnectionHandler{
 
-    private static HashSet<String> baseDoors = new HashSet<>();
+    private static Set<String> baseDoors = new HashSet<>();
     private static Map<Integer, String> doors = new HashMap<>();
 
     static void init() {
-        baseDoors.add("minecraft:oak_fence");
-        baseDoors.add("minecraft:birch_fence");
-        baseDoors.add("minecraft:jungle_fence");
-        baseDoors.add("minecraft:dark_oak_fence");
-        baseDoors.add("minecraft:acacia_fence");
-        baseDoors.add("minecraft:spruce_fence");
+        baseDoors.add("minecraft:oak_door");
+        baseDoors.add("minecraft:birch_door");
+        baseDoors.add("minecraft:jungle_door");
+        baseDoors.add("minecraft:dark_oak_door");
+        baseDoors.add("minecraft:acacia_door");
+        baseDoors.add("minecraft:spruce_door");
         baseDoors.add("minecraft:iron_door");
 
         DoorConnectionHandler connectionHandler = new DoorConnectionHandler();
