2ebba15904926a208de599c47cb06590b3c2742a
Marco Neuhaus <mneuhaus44@gmail.com>
2018-10-26 21:55:02 +0200

    fix

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/DoorConnectionHandler.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/DoorConnectionHandler.java
@@ -4,7 +4,6 @@ import us.myles.ViaVersion.api.data.UserConnection;
 import us.myles.ViaVersion.api.minecraft.BlockFace;
 import us.myles.ViaVersion.api.minecraft.Position;
 
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -12,7 +11,6 @@ import java.util.Set;
 public class DoorConnectionHandler extends ConnectionHandler{
 
     private static Set<String> baseDoors = new HashSet<>();
-    private static Map<Integer, String> doors = new HashMap<>();
 
     static void init() {
         baseDoors.add("minecraft:oak_door");
@@ -27,7 +25,6 @@ public class DoorConnectionHandler extends ConnectionHandler{
         for (Map.Entry<String, Integer> blockState : ConnectionData.keyToId.entrySet()) {
             String key = blockState.getKey().split("\\[")[0];
             if (baseDoors.contains(key)) {
-                doors.put(blockState.getValue(), key);
                 ConnectionData.connectionHandlerMap.put(blockState.getValue(), connectionHandler);
             }
         }
@@ -38,15 +35,15 @@ public class DoorConnectionHandler extends ConnectionHandler{
         int blockBelowId = getBlockData(user, position.getRelative(BlockFace.BOTTOM));
         int blockAboveId = getBlockData(user, position.getRelative(BlockFace.TOP));
         WrappedBlockData blockdata = WrappedBlockData.fromStateId(blockState);
-        if(doors.containsKey(blockState)){
+        if(canConnect(blockState)){
             if (blockdata.getValue("half").equals("lower")){
-                if(doors.containsKey(blockAboveId)){
+                if(canConnect(blockAboveId)){
                     WrappedBlockData blockAboveData = WrappedBlockData.fromStateId(blockAboveId);
                     blockdata.set("hinge", blockAboveData.getValue("hinge"));
                     blockdata.set("powered", blockAboveData.getValue("powered"));
                 }
             }else{
-                if(doors.containsKey(blockBelowId)){
+                if(canConnect(blockBelowId)){
                     WrappedBlockData blockBelowData = WrappedBlockData.fromStateId(blockBelowId);
                     blockdata.set("open", blockBelowData.getValue("open"));
                     blockdata.set("facing", blockBelowData.getValue("facing"));
