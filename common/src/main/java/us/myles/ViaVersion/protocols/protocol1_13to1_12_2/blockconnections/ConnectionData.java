dc466d3e829cbeb8fdd89beb4200eb4cfba749f9
Marco Neuhaus <mneuhaus44@gmail.com>
2018-10-26 21:03:16 +0200

    format code

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/ConnectionData.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/ConnectionData.java
@@ -18,94 +18,93 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
-public class ConnectionData{
-	static Map<Integer, String> idToKey = new HashMap<>();
-	static Map<String, Integer> keyToId = new HashMap<>();
-	static Map<Integer, ConnectionHandler> connectionHandlerMap = new HashMap<>();
-	static Map<Integer, BlockData> blockConnectionData = new HashMap<>();
+public class ConnectionData {
 
-	public static void update(UserConnection user, Position position) {
-		for (int x = -1; x <= 1; x++) {
-			for (int z = -1; z <= 1; z++) {
-				for (int y = -1; y <= 1; y++) {
-					if (Math.abs(x) + Math.abs(y) + Math.abs(z) != 1) continue;
-					Position pos = new Position(position.getX() + x, position.getY() + y, position.getZ() + z);
-					int blockState = Via.getManager().getProviders().get(BlockConnectionProvider.class).getBlockdata(user, pos);
-					if (!connects(blockState)) continue;
-					int newBlockState = connect(user, pos, blockState);
-					if (newBlockState == blockState) continue;
+    static Map<Integer, String> idToKey = new HashMap<>();
+    static Map<String, Integer> keyToId = new HashMap<>();
+    static Map<Integer, ConnectionHandler> connectionHandlerMap = new HashMap<>();
+    static Map<Integer, BlockData> blockConnectionData = new HashMap<>();
 
+    public static void update(UserConnection user, Position position) {
+        for (int x = -1; x <= 1; x++) {
+            for (int z = -1; z <= 1; z++) {
+                for (int y = -1; y <= 1; y++) {
+                    if (Math.abs(x) + Math.abs(y) + Math.abs(z) != 1) continue;
+                    Position pos = new Position(position.getX() + x, position.getY() + y, position.getZ() + z);
+                    int blockState = Via.getManager().getProviders().get(BlockConnectionProvider.class).getBlockdata(user, pos);
+                    if (!connects(blockState)) continue;
+                    int newBlockState = connect(user, pos, blockState);
+                    if (newBlockState == blockState) continue;
 
-					PacketWrapper blockUpdatePacket = new PacketWrapper(0x0B, null, user);
-					blockUpdatePacket.write(Type.POSITION, pos);
-					blockUpdatePacket.write(Type.VAR_INT, newBlockState);
-					try {
-						blockUpdatePacket.send(Protocol1_13To1_12_2.class, true, false);
-					} catch (Exception ex) {
-						ex.printStackTrace();
-					}
-				}
-			}
-		}
-	}
+                    PacketWrapper blockUpdatePacket = new PacketWrapper(0x0B, null, user);
+                    blockUpdatePacket.write(Type.POSITION, pos);
+                    blockUpdatePacket.write(Type.VAR_INT, newBlockState);
+                    try {
+                        blockUpdatePacket.send(Protocol1_13To1_12_2.class, true, false);
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 
-	public static void connectBlocks(UserConnection user, Chunk chunk) {
-		long xOff = chunk.getX() << 4;
-		long zOff = chunk.getZ() << 4;
+    public static void connectBlocks(UserConnection user, Chunk chunk) {
+        long xOff = chunk.getX() << 4;
+        long zOff = chunk.getZ() << 4;
 
-		for (int i = 0; i < chunk.getSections().length; i++) {
-			ChunkSection section = chunk.getSections()[i];
-			if (section == null) continue;
+        for (int i = 0; i < chunk.getSections().length; i++) {
+            ChunkSection section = chunk.getSections()[i];
+            if (section == null) continue;
 
-			boolean willConnect = false;
+            boolean willConnect = false;
 
             for (int p = 0; p < section.getPalette().size(); p++) {
                 int id = section.getPalette().get(p);
-                if(ConnectionData.connects(id)){
+                if (ConnectionData.connects(id)) {
                     willConnect = true;
                     break;
                 }
             }
+            if (!willConnect) continue;
 
-            if(willConnect){
-                long yOff = i << 4;
+            long yOff = i << 4;
 
-                for (int x = 0; x < 16; x++) {
-                    for (int y = 0; y < 16; y++) {
-                        for (int z = 0; z < 16; z++) {
-                            int block = section.getBlock(x, y, z);
+            for (int x = 0; x < 16; x++) {
+                for (int y = 0; y < 16; y++) {
+                    for (int z = 0; z < 16; z++) {
+                        int block = section.getBlock(x, y, z);
 
-                            if (ConnectionData.connects(block)) {
-                                block = ConnectionData.connect(user, new Position(xOff+ x, yOff + y, zOff + z), block);
-                                section.setFlatBlock(x, y, z, block);
-                            }
+                        if (ConnectionData.connects(block)) {
+                            block = ConnectionData.connect(user, new Position(xOff + x, yOff + y, zOff + z), block);
+                            section.setFlatBlock(x, y, z, block);
+                        }
 
-                            if (x == 0) {
-                                update(user, new Position(xOff - 1, yOff + y, zOff + z));
-                            } else if (x == 15) {
-                                update(user, new Position(xOff + 16, yOff + y, zOff + z));
-                            }
-                            if (z == 0) {
-                                update(user, new Position(xOff + x, yOff + y, zOff - 1));
-                            } else if (z == 15) {
-                                update(user, new Position(xOff + x, yOff + y, zOff + 16));
-                            }
+                        if (x == 0) {
+                            update(user, new Position(xOff - 1, yOff + y, zOff + z));
+                        } else if (x == 15) {
+                            update(user, new Position(xOff + 16, yOff + y, zOff + z));
+                        }
+                        if (z == 0) {
+                            update(user, new Position(xOff + x, yOff + y, zOff - 1));
+                        } else if (z == 15) {
+                            update(user, new Position(xOff + x, yOff + y, zOff + 16));
                         }
                     }
                 }
             }
-		}
-	}
+        }
+    }
 
-	public static void init() {
-		JsonObject mapping1_13 = MappingData.loadData("mapping-1.13.json");
-		JsonObject blocks1_13 = mapping1_13.getAsJsonObject("blocks");
-		for (Map.Entry<String, JsonElement> blockState : blocks1_13.entrySet()) {
-			Integer id = Integer.parseInt(blockState.getKey());
-			String key = blockState.getValue().getAsString();
-			idToKey.put(id, key);
-			keyToId.put(key, id);
-		}
+    public static void init() {
+        JsonObject mapping1_13 = MappingData.loadData("mapping-1.13.json");
+        JsonObject blocks1_13 = mapping1_13.getAsJsonObject("blocks");
+        for (Map.Entry<String, JsonElement> blockState : blocks1_13.entrySet()) {
+            Integer id = Integer.parseInt(blockState.getKey());
+            String key = blockState.getValue().getAsString();
+            idToKey.put(id, key);
+            keyToId.put(key, id);
+        }
 
         JsonObject mappingBlockConnections = MappingData.loadData("blockConnections.json");
         for (Entry<String, JsonElement> entry : mappingBlockConnections.entrySet()) {
@@ -117,9 +116,9 @@ public class ConnectionData{
                 Boolean[] data = new Boolean[6];
                 for (BlockFace value : BlockFace.values()) {
                     String face = value.toString().toLowerCase();
-                    if(object.has(face)){
+                    if (object.has(face)) {
                         data[value.ordinal()] = object.getAsJsonPrimitive(face).getAsBoolean();
-                    }else{
+                    } else {
                         data[value.ordinal()] = false;
                     }
                 }
@@ -137,34 +136,34 @@ public class ConnectionData{
         WallConnectionHandler.init();
         MelonConnectionHandler.init();
         GlassConnectionHandler.init();
-		ChestConnectionHandler.init();
-		DoorConnectionHandler.init();
-		RedstoneConnectionHandler.init();
-		StairConnectionHandler.init();
-	}
+        ChestConnectionHandler.init();
+        DoorConnectionHandler.init();
+        RedstoneConnectionHandler.init();
+        StairConnectionHandler.init();
+    }
 
-	public static boolean isWelcome(int blockState) {
-		return blockConnectionData.containsKey(blockState) || connectionHandlerMap.containsKey(blockState);
-	}
+    public static boolean isWelcome(int blockState) {
+        return blockConnectionData.containsKey(blockState) || connectionHandlerMap.containsKey(blockState);
+    }
 
-	public static boolean connects(int blockState) {
-		return connectionHandlerMap.containsKey(blockState);
-	}
+    public static boolean connects(int blockState) {
+        return connectionHandlerMap.containsKey(blockState);
+    }
 
-	public static int connect(UserConnection user, Position position, int blockState) {
-		if (connectionHandlerMap.containsKey(blockState)) {
-		    ConnectionHandler handler = connectionHandlerMap.get(blockState);
-			return handler.connect(user, position, blockState);
-		} else {
-			return blockState;
-		}
-	}
+    public static int connect(UserConnection user, Position position, int blockState) {
+        if (connectionHandlerMap.containsKey(blockState)) {
+            ConnectionHandler handler = connectionHandlerMap.get(blockState);
+            return handler.connect(user, position, blockState);
+        } else {
+            return blockState;
+        }
+    }
 
-	public static int getId(String key) {
-		return keyToId.containsKey(key) ? keyToId.get(key) : -1;
-	}
+    public static int getId(String key) {
+        return keyToId.containsKey(key) ? keyToId.get(key) : -1;
+    }
 
-	public static String getKey(int id) {
-		return idToKey.get(id);
-	}
+    public static String getKey(int id) {
+        return idToKey.get(id);
+    }
 }
