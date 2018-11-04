f560e9a540cb0a25427623e843dd7c78649ceba3
KennyTV <28825609+KennyTV@users.noreply.github.com>
2018-11-04 17:37:48 +0100

    Use super method

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/GlassConnectionHandler.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/GlassConnectionHandler.java
@@ -1,11 +1,10 @@
 package us.myles.ViaVersion.protocols.protocol1_13to1_12_2.blockconnections;
 
 import us.myles.ViaVersion.api.data.UserConnection;
-import us.myles.ViaVersion.api.minecraft.BlockFace;
 import us.myles.ViaVersion.api.minecraft.Position;
 import us.myles.ViaVersion.api.protocol.ProtocolRegistry;
 
-public class GlassConnectionHandler extends AbstractFenceConnectionHandler{
+public class GlassConnectionHandler extends AbstractFenceConnectionHandler {
 
     static void init() {
         new GlassConnectionHandler("paneConnections", "minecraft:white_stained_glass_pane");
@@ -32,12 +31,9 @@ public class GlassConnectionHandler extends AbstractFenceConnectionHandler{
         super(blockConnections, key);
     }
 
+    @Override
     protected Byte getStates(UserConnection user, Position position, int blockState) {
-        byte states = 0;
-        if (connects(BlockFace.EAST, getBlockData(user, position.getRelative(BlockFace.EAST)))) states |= 1;
-        if (connects(BlockFace.NORTH, getBlockData(user, position.getRelative(BlockFace.NORTH)))) states |= 2;
-        if (connects(BlockFace.SOUTH, getBlockData(user, position.getRelative(BlockFace.SOUTH)))) states |= 4;
-        if (connects(BlockFace.WEST, getBlockData(user, position.getRelative(BlockFace.WEST)))) states |= 8;
+        final Byte states = super.getStates(user, position, blockState);
         return states == 0 && (ProtocolRegistry.SERVER_PROTOCOL <= 47 && ProtocolRegistry.SERVER_PROTOCOL != -1) ? 15 : states;
     }
 }
