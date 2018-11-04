3fe09240c031616fe64f784498513cc723b6924d
KennyTV <28825609+KennyTV@users.noreply.github.com>
2018-11-04 17:38:01 +0100

    Use super method

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/GlassConnectionHandler.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/GlassConnectionHandler.java
@@ -34,6 +34,6 @@ public class GlassConnectionHandler extends AbstractFenceConnectionHandler {
     @Override
     protected Byte getStates(UserConnection user, Position position, int blockState) {
         final Byte states = super.getStates(user, position, blockState);
-        return states == 0 && (ProtocolRegistry.SERVER_PROTOCOL <= 47 && ProtocolRegistry.SERVER_PROTOCOL != -1) ? 15 : states;
+        return states == 0 && (ProtocolRegistry.SERVER_PROTOCOL <= 47 && ProtocolRegistry.SERVER_PROTOCOL != -1) ? 0xF : states;
     }
 }
