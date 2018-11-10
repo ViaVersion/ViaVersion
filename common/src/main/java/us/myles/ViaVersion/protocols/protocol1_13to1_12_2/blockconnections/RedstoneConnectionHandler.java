d1e9925e94c2849f9047985658f303e74a9a4c5a
Marco Neuhaus <mneuhaus44@gmail.com>
2018-11-10 14:51:04 +0100

    improve redstone performance

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/RedstoneConnectionHandler.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_13to1_12_2/blockconnections/RedstoneConnectionHandler.java
@@ -4,6 +4,7 @@ import us.myles.ViaVersion.api.data.UserConnection;
 import us.myles.ViaVersion.api.minecraft.BlockFace;
 import us.myles.ViaVersion.api.minecraft.Position;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -11,6 +12,9 @@ import java.util.Set;
 public class RedstoneConnectionHandler extends ConnectionHandler {
 
     private static Set<Integer> redstone = new HashSet<>();
+
+    private static Map<Short, Integer> connectedBlockStates = new HashMap<>();
+    private static Map<Integer, Integer> powerMappings = new HashMap<>();
 
     static void init() {
         RedstoneConnectionHandler connectionHandler = new RedstoneConnectionHandler();
@@ -20,36 +24,60 @@ public class RedstoneConnectionHandler extends ConnectionHandler {
             if (redstoneKey.equals(key)) {
                 redstone.add(blockState.getValue());
                 ConnectionData.connectionHandlerMap.put(blockState.getValue(), connectionHandler);
+                WrappedBlockData blockData = WrappedBlockData.fromStateId(blockState.getValue());
+                connectedBlockStates.put(getStates(blockData), blockData.getBlockStateId());
+                powerMappings.put(blockData.getBlockStateId(), Integer.valueOf(blockData.getValue("power")));
             }
         }
     }
 
+    private static short getStates(WrappedBlockData data){
+        short b = 0;
+        b |= getState(data.getValue("east"));
+        b |= getState(data.getValue("north")) << 2;
+        b |= getState(data.getValue("south")) << 4;
+        b |= getState(data.getValue("west")) << 6;
+        b |= Integer.valueOf(data.getValue("power")) << 8;
+        return b;
+    }
+
+    private static int getState(String value)
+    {
+        switch (value){
+            case "none": return 0;
+            case "side" : return 1;
+            case "up" : return 2;
+            default: return 0;
+        }
+    }
 
     @Override
     public int connect(UserConnection user, Position position, int blockState) {
-        WrappedBlockData blockdata = WrappedBlockData.fromStateId(blockState);
-        blockdata.set("east", connects(user, position, BlockFace.EAST));
-        blockdata.set("north", connects(user, position, BlockFace.NORTH));
-        blockdata.set("south", connects(user, position, BlockFace.SOUTH));
-        blockdata.set("west", connects(user, position, BlockFace.WEST));
-        return blockdata.getBlockStateId();
+        short b = 0;
+        b |= connects(user, position, BlockFace.EAST);
+        b |= connects(user, position, BlockFace.NORTH) << 2;
+        b |= connects(user, position, BlockFace.SOUTH) << 4;
+        b |= connects(user, position, BlockFace.WEST) << 6;
+        b |= powerMappings.get(blockState) << 8;
+        final Integer newBlockState = connectedBlockStates.get(b);
+        return newBlockState == null ? blockState : newBlockState;
     }
 
-    private String connects(UserConnection user, Position position, BlockFace side) {
+    private int connects(UserConnection user, Position position, BlockFace side) {
         final Position relative = position.getRelative(side);
         int blockState = getBlockData(user, relative);
         if (connects(side, blockState)) {
-            return "side";
+            return 1; //side
         }
         int up = getBlockData(user, relative.getRelative(BlockFace.TOP));
         if (redstone.contains(up) && !ConnectionData.occludingStates.contains(getBlockData(user, position.getRelative(BlockFace.TOP)))) {
-            return "up";
+            return 2; //"up"
         }
         int down = getBlockData(user, relative.getRelative(BlockFace.BOTTOM));
         if (redstone.contains(down) && !ConnectionData.occludingStates.contains(getBlockData(user, relative))) {
-            return "side";
+            return 1; //side
         }
-        return "none";
+        return 0; //none
     }
 
     private boolean connects(BlockFace side, int blockState) {
