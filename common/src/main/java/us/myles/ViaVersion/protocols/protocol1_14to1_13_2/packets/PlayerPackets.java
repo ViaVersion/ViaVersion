284a462936079a66534ea352758600d058b621dd
Gerrygames <gecam59@gmail.com>
2018-12-12 20:48:52 +0100

    18w50a

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_14to1_13_2/packets/PlayerPackets.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_14to1_13_2/packets/PlayerPackets.java
@@ -1,6 +1,8 @@
 package us.myles.ViaVersion.protocols.protocol1_14to1_13_2.packets;
 
+import us.myles.ViaVersion.api.PacketWrapper;
 import us.myles.ViaVersion.api.protocol.Protocol;
+import us.myles.ViaVersion.api.remapper.PacketHandler;
 import us.myles.ViaVersion.api.remapper.PacketRemapper;
 import us.myles.ViaVersion.api.type.Type;
 import us.myles.ViaVersion.packets.State;
@@ -10,7 +12,7 @@ public class PlayerPackets {
     public static void register(Protocol protocol) {
 
         // Open Sign Editor
-        protocol.registerOutgoing(State.PLAY, 0x2C, 0x2C, new PacketRemapper() {
+        protocol.registerOutgoing(State.PLAY, 0x2C, 0x2D, new PacketRemapper() {
             @Override
             public void registerMap() {
                 map(Type.POSITION, Type.POSITION1_14);
@@ -33,6 +35,34 @@ public class PlayerPackets {
                 map(Type.VAR_INT);
                 map(Type.POSITION1_14, Type.POSITION);
                 map(Type.BYTE);
+            }
+        });
+
+        // Recipe Book Data
+        protocol.registerIncoming(State.PLAY, 0x1B, 0x1B, new PacketRemapper() {
+            @Override
+            public void registerMap() {
+                map(Type.VAR_INT);
+                handler(new PacketHandler() {
+                    @Override
+                    public void handle(PacketWrapper wrapper) throws Exception {
+                        int type = wrapper.get(Type.VAR_INT, 0);
+                        if (type == 0) {
+                            wrapper.passthrough(Type.STRING);
+                        } else if (type == 1) {
+                            wrapper.passthrough(Type.BOOLEAN); // Crafting Recipe Book Open
+                            wrapper.passthrough(Type.BOOLEAN); // Crafting Recipe Filter Active
+                            wrapper.passthrough(Type.BOOLEAN); // Smelting Recipe Book Open
+                            wrapper.passthrough(Type.BOOLEAN); // Smelting Recipe Filter Active
+
+                            // Unknown new booleans
+                            wrapper.read(Type.BOOLEAN);
+                            wrapper.read(Type.BOOLEAN);
+                            wrapper.read(Type.BOOLEAN);
+                            wrapper.read(Type.BOOLEAN);
+                        }
+                    }
+                });
             }
         });
 
