11b25151f7763f4626e54b90c1bb16183c8fa3f1
creeper123123321 <creeper123123321@gmail.com>
2019-02-13 17:00:01 -0200

    Remove uuid tracking

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_14to1_13_2/storage/EntityTracker.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_14to1_13_2/storage/EntityTracker.java
@@ -1,9 +1,6 @@
 package us.myles.ViaVersion.protocols.protocol1_14to1_13_2.storage;
 
 import com.google.common.base.Optional;
-import com.google.common.collect.BiMap;
-import com.google.common.collect.HashBiMap;
-import com.google.common.collect.Maps;
 import us.myles.ViaVersion.api.data.StoredObject;
 import us.myles.ViaVersion.api.data.UserConnection;
 import us.myles.ViaVersion.api.entities.Entity1_14Types;
@@ -14,19 +11,16 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class EntityTracker extends StoredObject {
     private final Map<Integer, Entity1_14Types.EntityType> clientEntityTypes = new ConcurrentHashMap<>();
-    private final BiMap<UUID, Integer> uuidToId = Maps.synchronizedBiMap(HashBiMap.<UUID, Integer>create());
 
     public EntityTracker(UserConnection user) {
         super(user);
     }
 
     public void removeEntity(int entityId) {
-        uuidToId.inverse().remove(entityId);
         clientEntityTypes.remove(entityId);
     }
 
     public void addEntity(int entityId, UUID uuid, Entity1_14Types.EntityType type) {
-        uuidToId.forcePut(uuid, entityId);
         clientEntityTypes.put(entityId, type);
     }
 
@@ -36,9 +30,5 @@ public class EntityTracker extends StoredObject {
 
     public Optional<Entity1_14Types.EntityType> get(int id) {
         return Optional.fromNullable(clientEntityTypes.get(id));
-    }
-
-    public Optional<UUID> getUUID(int id) {
-        return Optional.fromNullable(uuidToId.inverse().get(id));
     }
 }
