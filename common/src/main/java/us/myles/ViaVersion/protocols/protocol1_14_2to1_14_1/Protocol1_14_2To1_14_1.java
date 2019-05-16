a075e31a4e26c8c3922f5c902e9689ed70b142cb
Gerrygames <gecam59@gmail.com>
2019-05-16 19:21:26 +0200

    1.14.2-pre1

---
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_14_2to1_14_1/Protocol1_14_2To1_14_1.java
@@ -0,0 +1,17 @@
+package us.myles.ViaVersion.protocols.protocol1_14_2to1_14_1;
+
+import us.myles.ViaVersion.api.data.UserConnection;
+import us.myles.ViaVersion.api.protocol.Protocol;
+
+public class Protocol1_14_2To1_14_1 extends Protocol {
+
+	@Override
+	protected void registerPackets() {
+
+	}
+
+	@Override
+	public void init(UserConnection userConnection) {
+
+	}
+}
