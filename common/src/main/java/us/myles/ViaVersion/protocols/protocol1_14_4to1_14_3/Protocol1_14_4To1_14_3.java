bc09187dc1fc1626251f0e8f00cd352679573101
KennyTV <28825609+KennyTV@users.noreply.github.com>
2019-07-15 14:58:55 +0200

    1.14.4-pre6 support

--- common/src/main/java/us/myles/ViaVersion/protocols/protocol1_14_4to1_14_3/Protocol1_14_4To1_14_3.java
+++ common/src/main/java/us/myles/ViaVersion/protocols/protocol1_14_4to1_14_3/Protocol1_14_4To1_14_3.java
@@ -1,12 +1,44 @@
 package us.myles.ViaVersion.protocols.protocol1_14_4to1_14_3;
 
+import us.myles.ViaVersion.api.PacketWrapper;
 import us.myles.ViaVersion.api.data.UserConnection;
 import us.myles.ViaVersion.api.protocol.Protocol;
+import us.myles.ViaVersion.api.remapper.PacketHandler;
+import us.myles.ViaVersion.api.remapper.PacketRemapper;
+import us.myles.ViaVersion.api.type.Type;
+import us.myles.ViaVersion.packets.State;
 
 public class Protocol1_14_4To1_14_3 extends Protocol {
 
     @Override
     protected void registerPackets() {
+        // trade list
+        registerOutgoing(State.PLAY, 0x27, 0x27, new PacketRemapper() {
+            @Override
+            public void registerMap() {
+                handler(new PacketHandler() {
+                    @Override
+                    public void handle(PacketWrapper wrapper) throws Exception {
+                        wrapper.passthrough(Type.VAR_INT);
+                        int size = wrapper.passthrough(Type.UNSIGNED_BYTE);
+                        for (int i = 0; i < size; i++) {
+                            wrapper.passthrough(Type.FLAT_VAR_INT_ITEM);
+                            wrapper.passthrough(Type.FLAT_VAR_INT_ITEM);
+                            if (wrapper.passthrough(Type.BOOLEAN)) {
+                                wrapper.passthrough(Type.FLAT_VAR_INT_ITEM);
+                            }
+                            wrapper.passthrough(Type.BOOLEAN);
+                            wrapper.passthrough(Type.INT);
+                            wrapper.passthrough(Type.INT);
+                            wrapper.passthrough(Type.INT);
+                            wrapper.passthrough(Type.INT);
+                            wrapper.passthrough(Type.FLOAT);
+                            wrapper.write(Type.INT, 0); // demand value added in pre5
+                        }
+                    }
+                });
+            }
+        });
     }
 
     @Override
