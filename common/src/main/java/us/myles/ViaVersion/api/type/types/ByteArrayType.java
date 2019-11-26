915258169ed0444c8a0c3fd7deb64e506e4ff73b
KennyTV <28825609+KennyTV@users.noreply.github.com>
2019-11-26 13:51:37 +0100

    Fix inverted array read check

--- common/src/main/java/us/myles/ViaVersion/api/type/types/ByteArrayType.java
+++ common/src/main/java/us/myles/ViaVersion/api/type/types/ByteArrayType.java
@@ -18,7 +18,7 @@ public class ByteArrayType extends Type<byte[]> {
     @Override
     public byte[] read(ByteBuf buffer) throws Exception {
         int length = Type.VAR_INT.read(buffer);
-        Preconditions.checkArgument(!buffer.isReadable(length), "Length is fewer than readable bytes");
+        Preconditions.checkArgument(buffer.isReadable(length), "Length is fewer than readable bytes");
         byte[] array = new byte[length];
         buffer.readBytes(array);
         return array;
