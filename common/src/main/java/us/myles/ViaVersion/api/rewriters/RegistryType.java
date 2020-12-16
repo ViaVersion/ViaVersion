823ac0e173312c90696421c279e61de696e383b1
KennyTV <jahnke.nassim@gmail.com>
2020-12-16 19:50:49 +0100

    Add RegistryType#getByKey

--- common/src/main/java/us/myles/ViaVersion/api/rewriters/RegistryType.java
+++ common/src/main/java/us/myles/ViaVersion/api/rewriters/RegistryType.java
@@ -1,5 +1,10 @@
 package us.myles.ViaVersion.api.rewriters;
 
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public enum RegistryType {
 
     BLOCK("block"),
@@ -8,11 +13,23 @@ public enum RegistryType {
     ENTITY("entity_type"),
     GAME_EVENT("game_event");
 
+    private static final Map<String, RegistryType> MAP = new HashMap<>();
+    private static final RegistryType[] VALUES = values();
+
+    static {
+        for (RegistryType type : getValues()) {
+            MAP.put(type.resourceLocation, type);
+        }
+    }
+
     public static RegistryType[] getValues() {
         return VALUES;
     }
 
-    private static final RegistryType[] VALUES = values();
+    @Nullable
+    public static RegistryType getByKey(String resourceKey) {
+        return MAP.get(resourceKey);
+    }
 
     private final String resourceLocation;
 
