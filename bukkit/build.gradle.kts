c059e57771b493d4766f78ce22a94d73a272abe5
KennyTV <jahnke.nassim@gmail.com>
2021-02-18 11:34:22 +0100

    Move javassist to Bukkit moduke

--- bukkit/build.gradle.kts
+++ bukkit/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     implementation(project(":viaversion-bukkit-legacy"))
     implementation(project(":viaversion-common"))
+    implementation("org.javassist", "javassist", Versions.javassist)
     compileOnly("org.spigotmc", "spigot-api", Versions.spigot) {
         exclude("junit", "junit")
         exclude("com.google.code.gson", "gson")
