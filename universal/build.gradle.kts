03638028cd81acbb06af1c6389ddb5d5c224a91c
Jason <jasonpenilla2@me.com>
2021-05-25 00:44:57 -0700

    Make proper sources jar (#2495)

--- universal/build.gradle.kts
+++ universal/build.gradle.kts
@@ -4,27 +4,37 @@ plugins {
     id("com.github.johnrengelman.shadow")
 }
 
+val platforms = setOf(
+    rootProject.projects.viaversionBukkit,
+    rootProject.projects.viaversionBungee,
+    rootProject.projects.viaversionFabric,
+    rootProject.projects.viaversionSponge,
+    rootProject.projects.viaversionVelocity
+).map { it.dependencyProject }
+
 tasks {
     shadowJar {
         archiveClassifier.set("")
         archiveFileName.set("ViaVersion-${project.version}.jar")
         destinationDirectory.set(rootProject.projectDir.resolve("build/libs"))
         duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        sequenceOf(
-            rootProject.projects.viaversionBukkit,
-            rootProject.projects.viaversionBungee,
-            rootProject.projects.viaversionFabric,
-            rootProject.projects.viaversionSponge,
-            rootProject.projects.viaversionVelocity,
-        ).map { it.dependencyProject }.forEach { subproject ->
-            val shadowJarTask = subproject.tasks.named<ShadowJar>("shadowJar").forUseAtConfigurationTime().get()
+        platforms.forEach { platform ->
+            val shadowJarTask = platform.tasks.named<ShadowJar>("shadowJar").forUseAtConfigurationTime().get()
             dependsOn(shadowJarTask)
-            dependsOn(subproject.tasks.withType<Jar>())
+            dependsOn(platform.tasks.withType<Jar>())
             from(zipTree(shadowJarTask.archiveFile))
         }
     }
     build {
         dependsOn(shadowJar)
+    }
+    sourcesJar {
+        rootProject.subprojects.forEach { subproject ->
+            if (subproject == project) return@forEach
+            val platformSourcesJarTask = subproject.tasks.findByName("sourcesJar") as? Jar ?: return@forEach
+            dependsOn(platformSourcesJarTask)
+            from(zipTree(platformSourcesJarTask.archiveFile))
+        }
     }
 }
 
