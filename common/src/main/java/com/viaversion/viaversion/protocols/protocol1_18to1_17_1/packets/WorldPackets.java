69ccb2edfd6b23aabd78ff501b95911dd807d749
Bobcat00 <Bobcat00@users.noreply.github.com>
2022-01-08 19:08:27 -0500

    Allow "No light data found" message to be suppressed This uses the existing suppress-conversion-warnings config entry to supress the "No light data found for chunk at... Chunk was already loaded..." warning. This message can occur if a plugin does asynchronous chunk processing and a chunk is unloaded before the plugin's processing is completed.

--- common/src/main/java/com/viaversion/viaversion/protocols/protocol1_18to1_17_1/packets/WorldPackets.java
+++ common/src/main/java/com/viaversion/viaversion/protocols/protocol1_18to1_17_1/packets/WorldPackets.java
@@ -168,7 +168,9 @@ public final class WorldPackets {
                     final ChunkLightStorage.ChunkLight light = Via.getConfig().cache1_17Light() ?
                             lightStorage.getLight(chunk.getX(), chunk.getZ()) : lightStorage.removeLight(chunk.getX(), chunk.getZ());
                     if (light == null) {
-                        Via.getPlatform().getLogger().warning("No light data found for chunk at " + chunk.getX() + ", " + chunk.getZ() + ". Chunk was already loaded: " + alreadyLoaded);
+                        if (!Via.getConfig().isSuppressConversionWarnings() || Via.getManager().isDebug()) {
+                            Via.getPlatform().getLogger().warning("No light data found for chunk at " + chunk.getX() + ", " + chunk.getZ() + ". Chunk was already loaded: " + alreadyLoaded);
+                        }
 
                         final BitSet emptyLightMask = new BitSet();
                         emptyLightMask.set(0, tracker.currentWorldSectionHeight() + 2);
