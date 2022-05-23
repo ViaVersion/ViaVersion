1308dcb8dd2a10ea271e234f0dc85a9cac9ddc2a
Nassim Jahnke <jahnke.nassim@gmail.com>
2022-05-23 18:24:32 +0200

    No need to queue messages anymore The client now uses the main registry instead of the level registry

--- bungee/src/main/java/com/viaversion/viaversion/bungee/handlers/BungeeServerHandler.java
+++ bungee/src/main/java/com/viaversion/viaversion/bungee/handlers/BungeeServerHandler.java
@@ -48,9 +48,12 @@ import net.md_5.bungee.protocol.packet.PluginMessage;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.nio.charset.StandardCharsets;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
 
 public class BungeeServerHandler implements Listener {
     private static Method getHandshake;
