7adadc0d211bda938887803ba614130f6c0294cb
EnZaXD <60033407+FlorianMichael@users.noreply.github.com>
2023-05-03 09:17:51 +0200

    added PlayerAbilities Provider (#3311)

---
+++ common/src/main/java/com/viaversion/viaversion/protocols/protocol1_16to1_15_2/provider/PlayerAbilitiesProvider.java
@@ -0,0 +1,31 @@
+/*
+ * This file is part of ViaVersion - https://github.com/ViaVersion/ViaVersion
+ * Copyright (C) 2023 ViaVersion and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.viaversion.viaversion.protocols.protocol1_16to1_15_2.provider;
+
+import com.viaversion.viaversion.api.platform.providers.Provider;
+
+public class PlayerAbilitiesProvider implements Provider {
+
+    public float getFlyingSpeed() {
+        return 0.05F;
+    }
+
+    public float getWalkingSpeed() {
+        return 0.1F;
+    }
+}
