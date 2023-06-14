1005a0a61e617115ebb2a06d88239f14746127cb
Kichura <68134602+Kichura@users.noreply.github.com>
2023-06-14 19:32:00 +0200

    Update Blossom to 1.3.1.
    Seems like 1.3.1 is available but maven's repo1 list did not pick it up for some reason.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
 pluginManagement {
     // default plugin versions
     plugins {
-        id("net.kyori.blossom") version "1.3.0"
+        id("net.kyori.blossom") version "1.3.1"
         id("com.github.johnrengelman.shadow") version "8.1.1"
     }
 }
