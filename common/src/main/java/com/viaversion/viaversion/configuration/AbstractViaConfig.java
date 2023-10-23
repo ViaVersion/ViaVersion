39b5dc3e0b2abd102a3a1578f6263b1bf1e5d4c8
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-10-23 20:59:23 +0200

    Delete unused config option and added missing documentation

--- common/src/main/java/com/viaversion/viaversion/configuration/AbstractViaConfig.java
+++ common/src/main/java/com/viaversion/viaversion/configuration/AbstractViaConfig.java
@@ -89,7 +89,6 @@ public abstract class AbstractViaConfig extends Config implements ViaVersionConf
     private JsonElement resourcePack1_17PromptMessage;
     private WorldIdentifiers map1_16WorldNames;
     private boolean cache1_17Light;
-    private Map<String, String> chatTypeFormats;
 
     protected AbstractViaConfig(final File configFile) {
         super(configFile);
@@ -158,7 +157,6 @@ public abstract class AbstractViaConfig extends Config implements ViaVersionConf
                 worlds.getOrDefault("nether", WorldIdentifiers.NETHER_DEFAULT),
                 worlds.getOrDefault("end", WorldIdentifiers.END_DEFAULT));
         cache1_17Light = getBoolean("cache-1_17-light", true);
-        chatTypeFormats = get("chat-types-1_19", Map.class, new HashMap<String, String>());
     }
 
     private BlockedProtocolVersions loadBlockedProtocolVersions() {
@@ -526,11 +524,6 @@ public abstract class AbstractViaConfig extends Config implements ViaVersionConf
     @Override
     public boolean cache1_17Light() {
         return cache1_17Light;
-    }
-
-    @Override
-    public @Nullable String chatTypeFormat(final String translationKey) {
-        return chatTypeFormats.get(translationKey);
     }
 
     @Override
