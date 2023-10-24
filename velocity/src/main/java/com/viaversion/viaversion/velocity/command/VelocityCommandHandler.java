3f426568d1d83c85431ee5b6e56b247fda7e13ec
FlorianMichael <60033407+FlorianMichael@users.noreply.github.com>
2023-10-24 05:28:49 +0200

    Use viaversion.command as basement permission for the root command

--- velocity/src/main/java/com/viaversion/viaversion/velocity/command/VelocityCommandHandler.java
+++ velocity/src/main/java/com/viaversion/viaversion/velocity/command/VelocityCommandHandler.java
@@ -43,6 +43,6 @@ public class VelocityCommandHandler extends ViaCommandHandler implements SimpleC
 
     @Override
     public boolean hasPermission(Invocation invocation) {
-        return true;
+        return invocation.source().hasPermission("viaversion.command"); // The permission is also referenced here to filter root suggestions (/via<tab>)
     }
 }
