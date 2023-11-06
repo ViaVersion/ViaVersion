ffdddeeb0aa82aaa244fffeb153183d34f71f1d8
Nassim Jahnke <nassim@njahnke.dev>
2023-11-06 14:52:37 +1000

    Add back sources collection in universal jar publishing

--- universal/build.gradle.kts
+++ universal/build.gradle.kts
@@ -19,6 +19,14 @@ tasks {
         archiveFileName.set("ViaVersion-${project.version}.jar")
         destinationDirectory.set(rootProject.projectDir.resolve("build/libs"))
     }
+    sourcesJar {
+        rootProject.subprojects.forEach { subproject ->
+            if (subproject == project) return@forEach
+            val platformSourcesJarTask = subproject.tasks.findByName("sourcesJar") as? Jar ?: return@forEach
+            dependsOn(platformSourcesJarTask)
+            from(zipTree(platformSourcesJarTask.archiveFile))
+        }
+    }
 }
 
 val branch = rootProject.branchName()
