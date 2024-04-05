617ef1066734c18173385d991a21d75e99b01be1
EnZaXD <60033407+FlorianMichael@users.noreply.github.com>
2024-04-05 19:48:59 +0200

    Remove MetaType1_8#NonExistent (#3774)

--- api/src/main/java/com/viaversion/viaversion/api/minecraft/metadata/types/MetaType1_8.java
+++ api/src/main/java/com/viaversion/viaversion/api/minecraft/metadata/types/MetaType1_8.java
@@ -24,7 +24,6 @@ package com.viaversion.viaversion.api.minecraft.metadata.types;
 
 import com.viaversion.viaversion.api.minecraft.metadata.MetaType;
 import com.viaversion.viaversion.api.type.Type;
-import com.viaversion.viaversion.api.type.types.VoidType;
 
 public enum MetaType1_8 implements MetaType {
     Byte(0, Type.BYTE),
@@ -34,9 +33,7 @@ public enum MetaType1_8 implements MetaType {
     String(4, Type.STRING),
     Slot(5, Type.ITEM1_8),
     Position(6, Type.VECTOR),
-    Rotation(7, Type.ROTATION),
-    @Deprecated
-    NonExistent(-1, new VoidType());
+    Rotation(7, Type.ROTATION);
 
     private final int typeID;
     private final Type type;
