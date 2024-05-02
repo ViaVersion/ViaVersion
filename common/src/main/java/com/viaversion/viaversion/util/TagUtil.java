d3af70d76c2fb5e3f99d7eee15c9acdaa5bd7000
EnZaXD <60033407+FlorianMichael@users.noreply.github.com>
2024-05-02 15:45:19 +0200

    Add optional registry utils to TagUtil (#3819)

--- common/src/main/java/com/viaversion/viaversion/util/TagUtil.java
+++ common/src/main/java/com/viaversion/viaversion/util/TagUtil.java
@@ -26,19 +26,33 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 public final class TagUtil {
 
     public static ListTag<CompoundTag> getRegistryEntries(final CompoundTag tag, final String key) {
+        return getRegistryEntries(tag, key, null);
+    }
+
+    public static ListTag<CompoundTag> getRegistryEntries(final CompoundTag tag, final String key, final @Nullable ListTag<CompoundTag> defaultValue) {
         CompoundTag registry = tag.getCompoundTag(Key.namespaced(key));
         if (registry == null) {
             registry = tag.getCompoundTag(Key.stripMinecraftNamespace(key));
+            if (registry == null) {
+                return defaultValue;
+            }
         }
         return registry.getListTag("value", CompoundTag.class);
     }
 
     public static ListTag<CompoundTag> removeRegistryEntries(final CompoundTag tag, final String key) {
+        return removeRegistryEntries(tag, key, null);
+    }
+
+    public static ListTag<CompoundTag> removeRegistryEntries(final CompoundTag tag, final String key, final @Nullable ListTag<CompoundTag> defaultValue) {
         String currentKey = Key.namespaced(key);
         CompoundTag registry = tag.getCompoundTag(currentKey);
         if (registry == null) {
             currentKey = Key.stripMinecraftNamespace(key);
             registry = tag.getCompoundTag(currentKey);
+            if (registry == null) {
+                return defaultValue;
+            }
         }
         tag.remove(currentKey);
         return registry.getListTag("value", CompoundTag.class);
