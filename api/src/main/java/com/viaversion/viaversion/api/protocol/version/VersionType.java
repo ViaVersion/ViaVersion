9f6e7fa4e58449ad2098e05d1f4625f30ab54981
Nassim Jahnke <nassim@njahnke.dev>
2025-01-01 12:31:22 +0100

    [ci skip] Update copyright header

--- api/src/main/java/com/viaversion/viaversion/api/protocol/version/VersionType.java
+++ api/src/main/java/com/viaversion/viaversion/api/protocol/version/VersionType.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of ViaVersion - https://github.com/ViaVersion/ViaVersion
- * Copyright (C) 2016-2024 ViaVersion and contributors
+ * Copyright (C) 2016-2025 ViaVersion and contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
