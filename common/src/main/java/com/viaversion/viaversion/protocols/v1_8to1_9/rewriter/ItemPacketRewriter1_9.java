ViaVersion/ViaVersion

commit a0b0ed4b68f8a19d12fb6d9aabba89381f5c7b8b

Make <1.21.2 container id types consistent with Vanilla again ... that's to say, to make them inconsistent thanks to special ids: Byte: - CONTAINER_SET_SLOT - CONTAINER_CLICK - PLACE_GHOST_RECIPE - PLACE_RECIPE - CONTAINER_BUTTON_CLICK - CONTAINER_CLOSE (serverbound) - CONTAINER_ACK (serverbound) Unsigned byte (unchanged): - OPEN_SCREEN - CONTAINER_SET_CONTENT - CONTAINER_SET_DATA - HORSE_SCREEN_OPEN - CONTAINER_CLOSE (clientbound) - CONTAINER_ACK (clientbound) Short (unchanged): - SET_CREATIVE_MODE_SLOT
This doesn't change much in practice, but means it matches the Vanilla reading

common/src/main/java/com/viaversion/viaversion/protocols/v1_8to1_9/rewriter/ItemPacketRewriter1_9.java
@@ -106,7 +106,7 @@ public class ItemPacketRewriter1_9 extends ItemRewriter<ClientboundPackets1_8, S
         protocol.registerClientbound(ClientboundPackets1_8.CONTAINER_SET_SLOT, new PacketHandlers() {
             @Override
             public void register() {
-                map(Types.UNSIGNED_BYTE); // 0 - Window ID
+                map(Types.BYTE); // 0 - Window ID
                 map(Types.SHORT); // 1 - Slot ID
                 map(Types.ITEM1_8); // 2 - Slot Value
                 handler(wrapper -> {
@@ -121,7 +121,7 @@ public class ItemPacketRewriter1_9 extends ItemRewriter<ClientboundPackets1_8, S
                         EntityTracker1_9 entityTracker = wrapper.user().getEntityTracker(Protocol1_8To1_9.class);
 
                         short slotID = wrapper.get(Types.SHORT, 0);
-                        byte windowId = wrapper.get(Types.UNSIGNED_BYTE, 0).byteValue();
+                        byte windowId = wrapper.get(Types.BYTE, 0);
 
                         // Store item in slot
                         inventoryTracker.setItemId(windowId, slotID, stack == null ? 0 : stack.identifier());
@@ -154,7 +154,7 @@ public class ItemPacketRewriter1_9 extends ItemRewriter<ClientboundPackets1_8, S
 
                 handler(wrapper -> {
                     Item[] stacks = wrapper.get(Types.ITEM1_8_SHORT_ARRAY, 0);
-                    Short windowId = wrapper.get(Types.UNSIGNED_BYTE, 0);
+                    short windowId = wrapper.get(Types.UNSIGNED_BYTE, 0);
 
                     InventoryTracker inventoryTracker = wrapper.user().get(InventoryTracker.class);
                     EntityTracker1_9 entityTracker = wrapper.user().getEntityTracker(Protocol1_8To1_9.class);
@@ -207,7 +207,8 @@ public class ItemPacketRewriter1_9 extends ItemRewriter<ClientboundPackets1_8, S
                 handler(wrapper -> {
                     InventoryTracker inventoryTracker = wrapper.user().get(InventoryTracker.class);
                     inventoryTracker.setInventory(null);
-                    inventoryTracker.resetInventory(wrapper.get(Types.UNSIGNED_BYTE, 0));
+                    short windowId = wrapper.get(Types.UNSIGNED_BYTE, 0);
+                    inventoryTracker.resetInventory(windowId);
                 });
             }
         });
@@ -245,7 +246,7 @@ public class ItemPacketRewriter1_9 extends ItemRewriter<ClientboundPackets1_8, S
                         short slotID = wrapper.get(Types.SHORT, 0);
 
                         // Update item in slot
-                        inventoryTracker.setItemId((short) 0, slotID, stack == null ? 0 : stack.identifier());
+                        inventoryTracker.setItemId(0, slotID, stack == null ? 0 : stack.identifier());
 
                         // Sync shield item in offhand with main hand
                         entityTracker.syncShieldWithSword();
@@ -260,7 +261,7 @@ public class ItemPacketRewriter1_9 extends ItemRewriter<ClientboundPackets1_8, S
                     if (throwItem) {
                         // Send a packet wiping the slot
                         wrapper.create(ClientboundPackets1_9.CONTAINER_SET_SLOT, w -> {
-                            w.write(Types.UNSIGNED_BYTE, (short) 0);
+                            w.write(Types.BYTE, (byte) 0);
                             w.write(Types.SHORT, slot);
                             w.write(Types.ITEM1_8, null);
                         }).send(Protocol1_8To1_9.class);
@@ -275,7 +276,7 @@ public class ItemPacketRewriter1_9 extends ItemRewriter<ClientboundPackets1_8, S
 
             @Override
             public void register() {
-                map(Types.UNSIGNED_BYTE); // 0 - Window ID
+                map(Types.BYTE); // 0 - Window ID
                 map(Types.SHORT); // 1 - Slot ID
                 map(Types.BYTE); // 2 - Button
                 map(Types.SHORT); // 3 - Action
@@ -285,10 +286,10 @@ public class ItemPacketRewriter1_9 extends ItemRewriter<ClientboundPackets1_8, S
                     Item stack = wrapper.get(Types.ITEM1_8, 0);
 
                     if (Via.getConfig().isShowShieldWhenSwordInHand()) {
-                        Short windowId = wrapper.get(Types.UNSIGNED_BYTE, 0);
-                        byte mode = wrapper.get(Types.BYTE, 1);
+                        byte windowId = wrapper.get(Types.BYTE, 0);
+                        byte mode = wrapper.get(Types.BYTE, 2);
                         short hoverSlot = wrapper.get(Types.SHORT, 0);
-                        byte button = wrapper.get(Types.BYTE, 0);
+                        byte button = wrapper.get(Types.BYTE, 1);
 
                         // Move items in inventory to track the sword location
                         InventoryTracker inventoryTracker = wrapper.user().get(InventoryTracker.class);
@@ -299,7 +300,7 @@ public class ItemPacketRewriter1_9 extends ItemRewriter<ClientboundPackets1_8, S
                 });
                 // Brewing patch and elytra throw patch
                 handler(wrapper -> {
-                    final short windowID = wrapper.get(Types.UNSIGNED_BYTE, 0);
+                    final byte windowID = wrapper.get(Types.BYTE, 0);
                     final short slot = wrapper.get(Types.SHORT, 0);
                     boolean throwItem = (slot == 45 && windowID == 0);
                     InventoryTracker inventoryTracker = wrapper.user().get(InventoryTracker.class);
@@ -315,7 +316,7 @@ public class ItemPacketRewriter1_9 extends ItemRewriter<ClientboundPackets1_8, S
                     if (throwItem) {
                         // Send a packet wiping the slot
                         wrapper.create(ClientboundPackets1_9.CONTAINER_SET_SLOT, w -> {
-                            w.write(Types.UNSIGNED_BYTE, windowID);
+                            w.write(Types.BYTE, windowID);
                             w.write(Types.SHORT, slot);
                             w.write(Types.ITEM1_8, null);
                         }).scheduleSend(Protocol1_8To1_9.class);
@@ -334,13 +335,13 @@ public class ItemPacketRewriter1_9 extends ItemRewriter<ClientboundPackets1_8, S
 
                 @Override
                 public void register() {
-                    map(Types.UNSIGNED_BYTE); // 0 - Window ID
+                    map(Types.BYTE); // 0 - Window ID
 
                     // Inventory tracking
                     handler(wrapper -> {
                         InventoryTracker inventoryTracker = wrapper.user().get(InventoryTracker.class);
                         inventoryTracker.setInventory(null);
-                        inventoryTracker.resetInventory(wrapper.get(Types.UNSIGNED_BYTE, 0));
+                        inventoryTracker.resetInventory(wrapper.get(Types.BYTE, 0));
                     });
                 }
             });

common/src/main/java/com/viaversion/viaversion/protocols/v1_11_1to1_12/rewriter/ItemPacketRewriter1_12.java
@@ -45,7 +45,7 @@ public class ItemPacketRewriter1_12 extends ItemRewriter<ClientboundPackets1_9_3
         protocol.registerServerbound(ServerboundPackets1_12.CONTAINER_CLICK, new PacketHandlers() {
                 @Override
                 public void register() {
-                    map(Types.UNSIGNED_BYTE); // 0 - Window ID
+                    map(Types.BYTE); // 0 - Window ID
                     map(Types.SHORT); // 1 - Slot
                     map(Types.BYTE); // 2 - Button
                     map(Types.SHORT); // 3 - Action number
@@ -58,11 +58,11 @@ public class ItemPacketRewriter1_12 extends ItemRewriter<ClientboundPackets1_9_3
                             handleItemToServer(wrapper.user(), item);
                             return;
                         }
-                        byte button = wrapper.get(Types.BYTE, 0);
+                        byte button = wrapper.get(Types.BYTE, 1);
                         int mode = wrapper.get(Types.VAR_INT, 0);
                         // QUICK_MOVE PATCH (Shift + (click/double click))
                         if (mode == 1 && button == 0 && item == null) {
-                            short windowId = wrapper.get(Types.UNSIGNED_BYTE, 0);
+                            short windowId = wrapper.get(Types.BYTE, 0);
                             short slotId = wrapper.get(Types.SHORT, 0);
                             short actionId = wrapper.get(Types.SHORT, 1);
                             InventoryQuickMoveProvider provider = Via.getManager().getProviders().get(InventoryQuickMoveProvider.class);

common/src/main/java/com/viaversion/viaversion/protocols/v1_12_2to1_13/Protocol1_12_2To1_13.java
@@ -353,7 +353,7 @@ public class Protocol1_12_2To1_13 extends AbstractProtocol<ClientboundPackets1_1
         registerClientbound(ClientboundPackets1_12_1.PLACE_GHOST_RECIPE, new PacketHandlers() {
             @Override
             public void register() {
-                map(Types.UNSIGNED_BYTE);
+                map(Types.BYTE);
                 handler(wrapper -> wrapper.write(Types.STRING, "viaversion:legacy/" + wrapper.read(Types.VAR_INT)));
             }
         });
@@ -594,7 +594,7 @@ public class Protocol1_12_2To1_13 extends AbstractProtocol<ClientboundPackets1_1
         registerServerbound(ServerboundPackets1_13.PLACE_RECIPE, new PacketHandlers() {
             @Override
             public void register() {
-                map(Types.UNSIGNED_BYTE); // Window id
+                map(Types.BYTE); // Window id
 
                 handler(wrapper -> {
                     String s = wrapper.read(Types.STRING);

common/src/main/java/com/viaversion/viaversion/protocols/v1_12_2to1_13/rewriter/ItemPacketRewriter1_13.java
@@ -59,7 +59,7 @@ public class ItemPacketRewriter1_13 extends ItemRewriter<ClientboundPackets1_12_
         protocol.registerClientbound(ClientboundPackets1_12_1.CONTAINER_SET_SLOT, new PacketHandlers() {
             @Override
             public void register() {
-                map(Types.UNSIGNED_BYTE); // 0 - Window ID
+                map(Types.BYTE); // 0 - Window ID
                 map(Types.SHORT); // 1 - Slot ID
                 map(Types.ITEM1_8, Types.ITEM1_13); // 2 - Slot Value
 
@@ -185,7 +185,7 @@ public class ItemPacketRewriter1_13 extends ItemRewriter<ClientboundPackets1_12_
         protocol.registerServerbound(ServerboundPackets1_13.CONTAINER_CLICK, new PacketHandlers() {
             @Override
             public void register() {
-                map(Types.UNSIGNED_BYTE); // 0 - Window ID
+                map(Types.BYTE); // 0 - Window ID
                 map(Types.SHORT); // 1 - Slot
                 map(Types.BYTE); // 2 - Button
                 map(Types.SHORT); // 3 - Action number

common/src/main/java/com/viaversion/viaversion/protocols/v1_13_1to1_13_2/rewriter/ItemPacketRewriter1_13_2.java
@@ -30,7 +30,7 @@ public class ItemPacketRewriter1_13_2 {
         protocol.registerClientbound(ClientboundPackets1_13.CONTAINER_SET_SLOT, new PacketHandlers() {
             @Override
             public void register() {
-                map(Types.UNSIGNED_BYTE); // 0 - Window ID
+                map(Types.BYTE); // 0 - Window ID
                 map(Types.SHORT); // 1 - Slot ID
                 map(Types.ITEM1_13, Types.ITEM1_13_2); // 2 - Slot Value
             }
@@ -116,7 +116,7 @@ public class ItemPacketRewriter1_13_2 {
         protocol.registerServerbound(ServerboundPackets1_13.CONTAINER_CLICK, new PacketHandlers() {
             @Override
             public void register() {
-                map(Types.UNSIGNED_BYTE); // 0 - Window ID
+                map(Types.BYTE); // 0 - Window ID
                 map(Types.SHORT); // 1 - Slot
                 map(Types.BYTE); // 2 - Button
                 map(Types.SHORT); // 3 - Action number

common/src/main/java/com/viaversion/viaversion/protocols/v1_13_2to1_14/rewriter/ItemPacketRewriter1_14.java
@@ -194,7 +194,7 @@ public class ItemPacketRewriter1_14 extends ItemRewriter<ClientboundPackets1_13,
             // Selecting trade now moves the items, we need to resync the inventory
             PacketWrapper resyncPacket = wrapper.create(ServerboundPackets1_13.CONTAINER_CLICK);
             EntityTracker1_14 tracker = wrapper.user().getEntityTracker(Protocol1_13_2To1_14.class);
-            resyncPacket.write(Types.UNSIGNED_BYTE, ((short) tracker.getLatestTradeWindowId())); // 0 - Window ID
+            resyncPacket.write(Types.BYTE, ((byte) tracker.getLatestTradeWindowId())); // 0 - Window ID
             resyncPacket.write(Types.SHORT, ((short) -999)); // 1 - Slot
             resyncPacket.write(Types.BYTE, (byte) 2); // 2 - Button - End left click
             resyncPacket.write(Types.SHORT, ((short) ThreadLocalRandom.current().nextInt())); // 3 - Action number

common/src/main/java/com/viaversion/viaversion/protocols/v1_15_2to1_16/rewriter/ItemPacketRewriter1_16.java
@@ -51,7 +51,7 @@ public class ItemPacketRewriter1_16 extends ItemRewriter<ClientboundPackets1_15,
         // clear cursor item to prevent client to try dropping it during navigation between multiple inventories causing arm swing
         PacketHandler cursorRemapper = wrapper -> {
             PacketWrapper clearPacket = wrapper.create(ClientboundPackets1_16.CONTAINER_SET_SLOT);
-            clearPacket.write(Types.UNSIGNED_BYTE, (short) -1);
+            clearPacket.write(Types.BYTE, (byte) -1);
             clearPacket.write(Types.SHORT, (short) -1);
             clearPacket.write(Types.ITEM1_13_2, null);
             clearPacket.send(Protocol1_15_2To1_16.class);

common/src/main/java/com/viaversion/viaversion/protocols/v1_16_4to1_17/rewriter/ItemPacketRewriter1_17.java
@@ -56,7 +56,7 @@ public final class ItemPacketRewriter1_17 extends ItemRewriter<ClientboundPacket
         protocol.registerServerbound(ServerboundPackets1_17.CONTAINER_CLICK, new PacketHandlers() {
             @Override
             public void register() {
-                map(Types.UNSIGNED_BYTE); // Window Id
+                map(Types.BYTE); // Window Id
                 map(Types.SHORT); // Slot
                 map(Types.BYTE); // Button
                 handler(wrapper -> wrapper.write(Types.SHORT, (short) 0)); // Action id - doesn't matter, as the sent out confirmation packet will be cancelled
@@ -110,10 +110,10 @@ public final class ItemPacketRewriter1_17 extends ItemRewriter<ClientboundPacket
             // Check extra bit for fast dismissal
             if ((id & (1 << 30)) != 0) {
                 // Decode our requested inventory acknowledgement
-                short inventoryId = (short) ((id >> 16) & 0xFF);
+                byte inventoryId = (byte) ((id >> 16) & 0xFF);
                 short confirmationId = (short) (id & 0xFFFF);
                 PacketWrapper packet = wrapper.create(ServerboundPackets1_16_2.CONTAINER_ACK);
-                packet.write(Types.UNSIGNED_BYTE, inventoryId);
+                packet.write(Types.BYTE, inventoryId);
                 packet.write(Types.SHORT, confirmationId);
                 packet.write(Types.BOOLEAN, true); // Accept
                 packet.sendToServer(Protocol1_16_4To1_17.class);

commit dec70be8ce8bf2aa9215aaeaa75341cbbfb634b6

Set correct data types in 1.8->1.9 CONTAINER_CLICK
Regression from https://github.com/ViaVersion/ViaVersion/commit/a0b0ed4b68f8a19d12fb6d9aabba89381f5c7b8b

Closes https://github.com/ViaVersion/ViaVersion/issues/4458

common/src/main/java/com/viaversion/viaversion/protocols/v1_8to1_9/rewriter/ItemPacketRewriter1_9.java
@@ -321,8 +321,8 @@ public class ItemPacketRewriter1_9 extends ItemRewriter<ClientboundPackets1_8, S
                             w.write(Types.ITEM1_8, null);
                         }).scheduleSend(Protocol1_8To1_9.class);
                         // Finally reset to simulate throwing item
-                        wrapper.set(Types.BYTE, 0, (byte) 0); // Set button to 0
-                        wrapper.set(Types.BYTE, 1, (byte) 0); // Set mode to 0
+                        wrapper.set(Types.BYTE, 1, (byte) 0); // Set button to 0
+                        wrapper.set(Types.BYTE, 2, (byte) 0); // Set mode to 0
                         wrapper.set(Types.SHORT, 0, (short) -999); // Set slot to -999
                     }
                 });

gradle.properties
@@ -1,5 +1,5 @@
 # Project properties - we put these here so they can be modified without causing a recompile of the build scripts
-projectVersion=5.3.2
+projectVersion=5.3.3-SNAPSHOT
 
 # Smile emoji
 mcVersions=1.21.5, 1.21.4, 1.21.3, 1.21.2, 1.21.1, 1.21, 1.20.6, 1.20.5, 1.20.4, 1.20.3, 1.20.2, 1.20.1, 1.20, 1.19.4, 1.19.3, 1.19.2, 1.19.1, 1.19, 1.18.2, 1.18.1, 1.18, 1.17.1, 1.17, 1.16.5, 1.16.4, 1.16.3, 1.16.2, 1.16.1, 1.16, 1.15.2, 1.15.1, 1.15, 1.14.4, 1.14.3, 1.14.2, 1.14.1, 1.14, 1.13.2, 1.13.1, 1.13, 1.12.2, 1.12.1, 1.12, 1.11.2, 1.11.1, 1.11, 1.10.2, 1.10.1, 1.10, 1.9.4, 1.9.3, 1.9.2, 1.9.1, 1.9, 1.8.9