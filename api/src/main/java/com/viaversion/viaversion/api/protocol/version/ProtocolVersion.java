d6c16677fccf5aca2df6c6b7d9ff489bffd0098c
Nassim Jahnke <nassim@njahnke.dev>
2025-04-22 16:07:31 +0200

    25w17a

--- api/src/main/java/com/viaversion/viaversion/api/protocol/version/ProtocolVersion.java
+++ api/src/main/java/com/viaversion/viaversion/api/protocol/version/ProtocolVersion.java
@@ -89,7 +89,7 @@ public class ProtocolVersion implements Comparable<ProtocolVersion> {
     public static final ProtocolVersion v1_21_2 = register(768, "1.21.2-1.21.3", new SubVersionRange("1.21", 2, 3));
     public static final ProtocolVersion v1_21_4 = register(769, "1.21.4");
     public static final ProtocolVersion v1_21_5 = register(770, "1.21.5");
-    public static final ProtocolVersion v1_22 = register(771, 246, "1.22");
+    public static final ProtocolVersion v1_22 = register(771, 247, "1.22");
     public static final ProtocolVersion unknown = new ProtocolVersion(VersionType.SPECIAL, -1, -1, "UNKNOWN", null);
 
     static {
