a6b219c0dfee2000d9273104cbba05c5db6768c4
FlorianMichael <florian.michael07@gmail.com>
2025-05-11 22:26:35 +0200

    [ci skip] Allow build if Git is not installed or the project is downloaded as code zip (#4477)

--- build-logic/src/main/kotlin/extensions.kt
+++ build-logic/src/main/kotlin/extensions.kt
@@ -1,6 +1,12 @@
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.api.provider.ListProperty
+import org.gradle.api.provider.ValueSource
+import org.gradle.api.provider.ValueSourceParameters
 import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.gradle.process.ExecOperations
+import java.io.ByteArrayOutputStream
+import javax.inject.Inject
 
 fun Project.latestCommitHash(): String {
     return runGitCommand(listOf("rev-parse", "--short", "HEAD"))
@@ -15,9 +21,33 @@ fun Project.branchName(): String {
 }
 
 fun Project.runGitCommand(args: List<String>): String {
-    return providers.exec {
-        commandLine = listOf("git") + args
-    }.standardOutput.asBytes.get().toString(Charsets.UTF_8).trim()
+    return providers.of(GitCommand::class.java) { parameters.args.set(args) }.getOrNull() ?: "unknown"
+}
+
+abstract class GitCommand : ValueSource<String, GitCommand.GitCommandParameters> {
+
+    @get:Inject
+    abstract val execOperations: ExecOperations
+
+    interface GitCommandParameters : ValueSourceParameters {
+        val args: ListProperty<String>
+    }
+
+    override fun obtain(): String? {
+        try {
+            val command = listOf("git") + parameters.args.get()
+            val output = ByteArrayOutputStream()
+            execOperations.exec {
+                commandLine = command
+                standardOutput = output
+                isIgnoreExitValue = true
+            }
+
+            return output.toString(Charsets.UTF_8).trim().takeIf { it.isNotBlank() }
+        } catch (e: Exception) {
+            return null
+        }
+    }
 }
 
 fun JavaPluginExtension.javaTarget(version: Int) {
