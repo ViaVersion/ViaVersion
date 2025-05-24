d0e12c8e3d25d04291e80c62af6367b74b53fa38
Packsolite <packsolite@gmail.com>
2025-05-25 00:49:13 +0200

    Don't use legacy data

--- bukkit/src/main/java/com/viaversion/viaversion/bukkit/providers/BukkitPickItemProvider.java
+++ bukkit/src/main/java/com/viaversion/viaversion/bukkit/providers/BukkitPickItemProvider.java
@@ -193,7 +193,7 @@ public class BukkitPickItemProvider extends PickItemProvider {
                 LegacyBlockToItem legacy = LegacyBlockToItem.getInstance();
                 return legacy != null ? (block, includeData) -> legacy.blockToItem(block) : (b, i) -> null;
             } else if (PaperViaInjector.hasMethod(Material.class, "isItem")) {
-                return (block, includeData) -> block.getType().isItem() ? new ItemStack(block.getType(), 1, (short) 0, block.getData()) : null;
+                return (block, includeData) -> block.getType().isItem() ? new ItemStack(block.getType()) : null;
             } else {
                 return (b, i) -> null;
             }
