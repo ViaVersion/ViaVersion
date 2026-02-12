ViaVersion/ViaVersion

commit 13fcce60594c81511f4c4d663a9f3ba6a4eac5fb

Use right method for sub 1.21.5 item_name/custom_name item_hover handling

common/src/main/java/com/viaversion/viaversion/rewriter/text/ComponentRewriterBase.java
@@ -352,11 +352,8 @@ public abstract class ComponentRewriterBase<C extends ClientboundPacketType> imp
             return;
         }
 
-        final Tag itemName = TagUtil.getNamespacedTag(componentsTag, "item_name");
-        processTag(connection, itemName);
-        final Tag customName = TagUtil.getNamespacedTag(componentsTag, "custom_name");
-        processTag(connection, customName);
-        handleLore(connection, componentsTag);
+        handleNestedComponent(connection, TagUtil.getNamespacedTag(componentsTag, "item_name"));
+        handleNestedComponent(connection, TagUtil.getNamespacedTag(componentsTag, "custom_name"));
         handleWrittenBookContents(connection, componentsTag);
 
         handleAttributeModifiers(componentsTag);
@@ -430,8 +427,8 @@ public abstract class ComponentRewriterBase<C extends ClientboundPacketType> imp
         }
 
         for (final CompoundTag compoundTag : pagesTag) {
-            handleNestedComponent(connection, compoundTag, "raw");
-            handleNestedComponent(connection, compoundTag, "filtered");
+            handleNestedComponent(connection, compoundTag.get("raw"));
+            handleNestedComponent(connection, compoundTag.get("filtered"));
         }
     }
 
@@ -472,7 +469,13 @@ public abstract class ComponentRewriterBase<C extends ClientboundPacketType> imp
             || tag.remove("!" + Key.stripMinecraftNamespace(key)) != null;
     }
 
-    protected abstract void handleNestedComponent(UserConnection connection, CompoundTag parent, String key);
+    /**
+     * Rewrites a nested text component. Stored as a regular tag in 1.21.5+, but is nested via snbt in string components before that.
+     *
+     * @param connection user connection
+     * @param tag        nested tag to handle
+     */
+    protected abstract void handleNestedComponent(UserConnection connection, @Nullable Tag tag);
 
     public enum ReadType {
 

common/src/main/java/com/viaversion/viaversion/rewriter/text/JsonNBTComponentRewriter.java
@@ -203,9 +203,8 @@ public class JsonNBTComponentRewriter<C extends ClientboundPacketType> extends C
     }
 
     @Override
-    protected void handleNestedComponent(final UserConnection connection, final CompoundTag parent, final String key) {
-        final StringTag tag = parent.getStringTag(key);
-        if (tag == null) {
+    protected void handleNestedComponent(final UserConnection connection, @Nullable final Tag tag) {
+        if (!(tag instanceof StringTag stringTag)) {
             return;
         }
 
@@ -213,10 +212,10 @@ public class JsonNBTComponentRewriter<C extends ClientboundPacketType> extends C
         final var input = inputSerializerVersion();
         final var output = outputSerializerVersion();
 
-        final Tag asTag = input.toTag(input.toComponent(tag.getValue()));
+        final Tag asTag = input.toTag(input.toComponent(stringTag.getValue()));
         processTag(connection, asTag);
 
-        tag.setValue(output.toString(output.toComponent(asTag)));
+        stringTag.setValue(output.toString(output.toComponent(asTag)));
     }
 
     @Override

common/src/main/java/com/viaversion/viaversion/rewriter/text/NBTComponentRewriter.java
@@ -26,6 +26,7 @@ import com.viaversion.viaversion.api.minecraft.item.data.ChatType;
 import com.viaversion.viaversion.api.protocol.Protocol;
 import com.viaversion.viaversion.api.protocol.packet.ClientboundPacketType;
 import com.viaversion.viaversion.api.type.Types;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 /**
  * Rewrites nbt serialized components from 1.21.5 upwards. Can also handle json components outside of hover events.
@@ -68,11 +69,8 @@ public class NBTComponentRewriter<C extends ClientboundPacketType> extends Compo
     }
 
     @Override
-    protected void handleNestedComponent(final UserConnection connection, final CompoundTag parent, final String key) {
-        final Tag tag = parent.get(key);
-        if (tag != null) {
-            processTag(connection, tag);
-        }
+    protected void handleNestedComponent(final UserConnection connection, @Nullable final Tag tag) {
+        processTag(connection, tag);
     }
 
     @Override

commit edf09137e2eb2efbda7ffb6043eef69cb6bbd6dd

Handle nested lore components in item_hover handling again
Fixes a regression in https://github.com/ViaVersion/ViaVersion/commit/13fcce60594c81511f4c4d663a9f3ba6a4eac5fb

Signed-off-by: Florian Reuth <git@florianreuth.de>

common/src/main/java/com/viaversion/viaversion/rewriter/text/ComponentRewriterBase.java
@@ -354,6 +354,7 @@ public abstract class ComponentRewriterBase<C extends ClientboundPacketType> imp
 
         handleNestedComponent(connection, TagUtil.getNamespacedTag(componentsTag, "item_name"));
         handleNestedComponent(connection, TagUtil.getNamespacedTag(componentsTag, "custom_name"));
+        handleLore(connection, componentsTag);
         handleWrittenBookContents(connection, componentsTag);
 
         handleAttributeModifiers(componentsTag);
@@ -411,7 +412,7 @@ public abstract class ComponentRewriterBase<C extends ClientboundPacketType> imp
         }
 
         for (final Tag lore : loreTag) {
-            processTag(connection, lore);
+            handleNestedComponent(connection, lore);
         }
     }
 