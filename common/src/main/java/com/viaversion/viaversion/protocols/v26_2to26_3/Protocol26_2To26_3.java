e1809674fd93eae90c8534aaab374fea295ec4a4
Florian Reuth <git@florianreuth.de>
2026-09-19 19:50:12 +0200

    Send brewing recipes for vanilla servers not syncing the inventory in 26.2->26.3 (#5056)

--- common/src/main/java/com/viaversion/viaversion/protocols/v26_2to26_3/Protocol26_2To26_3.java
+++ common/src/main/java/com/viaversion/viaversion/protocols/v26_2to26_3/Protocol26_2To26_3.java
@@ -62,7 +62,6 @@ import com.viaversion.viaversion.rewriter.RecipeDisplayRewriter;
 import com.viaversion.viaversion.rewriter.RegistryDataRewriter;
 import com.viaversion.viaversion.rewriter.TagRewriter;
 import com.viaversion.viaversion.rewriter.text.NBTComponentRewriter;
-
 import java.util.BitSet;
 
 import static com.viaversion.viaversion.util.ProtocolUtil.packetTypeMap;
@@ -171,6 +170,19 @@ public final class Protocol26_2To26_3 extends AbstractProtocol<ClientboundPacket
             wrapper.passthrough(ChatType.TYPE); // Chat Type
             componentRewriter.processTag(wrapper.user(), wrapper.passthrough(Types.TRUSTED_TAG)); // Name
             componentRewriter.processTag(wrapper.user(), wrapper.passthrough(Types.TRUSTED_OPTIONAL_TAG)); // Target Name
+        });
+
+        // 26.3 reads the items the brewing stand slots accept from the server, older clients have them hardcoded
+        appendClientbound(ClientboundPackets26_1.UPDATE_RECIPES, wrapper -> {
+            wrapper.resetReader();
+            final int size = wrapper.read(Types.VAR_INT);
+            wrapper.write(Types.VAR_INT, size + 2);
+
+            // Copies, as later protocols rewrite the ids in place
+            wrapper.write(Types.STRING, "minecraft:brewing_input");
+            wrapper.write(Types.VAR_INT_ARRAY_PRIMITIVE, MAPPINGS.brewingInputIds().clone());
+            wrapper.write(Types.STRING, "minecraft:brewing_reagent");
+            wrapper.write(Types.VAR_INT_ARRAY_PRIMITIVE, MAPPINGS.brewingReagentIds().clone());
         });
     }
 
