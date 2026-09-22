438f5f4a63c90b43eadd07014523072ce4b07e1b
LeonTG <leontg77@gmail.com>
2026-09-23 01:35:32 +0200

    Fix entity death event packets for 1.15+ players

---
+++ common/src/main/java/com/viaversion/viaversion/protocols/v1_14_4to1_15/storage/DeadStatusStorage.java
@@ -0,0 +1,36 @@
+/*
+ * This file is part of ViaVersion - https://github.com/ViaVersion/ViaVersion
+ * Copyright (C) 2016-2026 ViaVersion and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.viaversion.viaversion.protocols.v1_14_4to1_15.storage;
+
+import com.viaversion.viaversion.api.connection.StorableObject;
+
+public final class DeadStatusStorage implements StorableObject {
+    private boolean dead;
+
+    public DeadStatusStorage(boolean dead) {
+        this.setDead(dead);
+    }
+
+    public void setDead(boolean dead) {
+        this.dead = dead;
+    }
+
+    public boolean wasDead() {
+        return this.dead;
+    }
+}
